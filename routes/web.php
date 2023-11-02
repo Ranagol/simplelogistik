f91fd551f0323d6fc1ba233b875acb7b4261b243
Ranagol <andorhorvat@gmail.com>
2023-11-02 18:56:32 +0100

    Resource route problem solved.

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::resource('users', UserController::class);
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
