6d2a9f0c7e338981faa66f889343214870f9efa6
Ranagol <andorhorvat@gmail.com>
2023-11-06 03:55:14 +0100

    Vuetify removed from app.ts and vite.config.js

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -8,98 +8,7 @@ import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import { Head } from '@inertiajs/vue3';
 import Layout from './Shared/Layout.vue';
 
-// import Layout from './Shared/Layout.vue';
 
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-const customDarkTheme = {
-    dark: true,
-    colors: {
-        'primary': '#7367F0',//done
-        'on-primary': '#fff',
-        'secondary': '#A8AAAE',
-        'on-secondary': '#fff',
-        'success': '#28C76F',//done
-        'on-success': '#fff',
-        'info': '#00CFE8',//done
-        'on-info': '#fff',
-        'warning': '#FF9F43',//done
-        'on-warning': '#fff',
-        'error': '#EA5455',//done, used as danger
-        'background': '#25293C',
-        'on-background': '#D0D4F1',
-        'surface': '#2F3349',
-        'on-surface': '#D0D4F1',
-        'grey-50': '#26293A',
-        'grey-100': '#2F3349',
-        'grey-200': '#26293A',
-        'grey-300': '#4A5072',
-        'grey-400': '#5E6692',
-        'grey-500': '#7983BB',
-        'grey-600': '#AAB3DE',
-        'grey-700': '#B6BEE3',
-        'grey-800': '#CFD3EC',
-        'grey-900': '#E7E9F6',
-        'perfect-scrollbar-thumb': '#4A5072',
-        'skin-bordered-background': '#2f3349',
-        'skin-bordered-surface': '#2f3349',
-    },
-    variables: {
-        'code-color': '#d400ff',
-        'overlay-scrim-background': '#101121',
-        'tooltip-background': '#5E6692',
-        'border-color': '#D0D4F1',
-        'overlay-scrim-opacity': 0.6,
-        'hover-opacity': 0.04,
-        'focus-opacity': 0.12,
-        'selected-opacity': 0.06,
-        'activated-opacity': 0.16,
-        'pressed-opacity': 0.14,
-        'dragged-opacity': 0.1,
-        'disabled-opacity': 0.42,
-        'border-opacity': 0.16,
-        'high-emphasis-opacity': 0.78,
-        'medium-emphasis-opacity': 0.68,
-        'switch-opacity': 0.4,
-        'switch-disabled-track-opacity': 0.4,
-        'switch-disabled-thumb-opacity': 0.8,
-        'switch-checked-disabled-opacity': 0.3,
-
-        // Shadows
-        'shadow-key-umbra-color': '#0F1422',
-    },
-};
-
-const vuetify = createVuetify({
-    components,
-    directives,
-    theme: {
-        defaultTheme: "customDarkTheme",
-        themes: {
-            customDarkTheme,
-        },
-    },
-});
-
-// const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-
-// createInertiaApp({
-//     title: (title) => `${title} - ${appName}`,
-//     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
-//     setup({ el, App, props, plugin }) {
-//         createApp({ render: () => h(App, props) })
-//             .use(plugin)
-//             .use(ZiggyVue, Ziggy)
-//             .mount(el);
-//     },
-//     progress: {
-//         color: '#4B5563',
-//     },
-// });
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -149,7 +58,6 @@ createInertiaApp({
             .component('Link', Link)//global registration of Link
             .component('Head', Head)//global registration of Head
             .use(ZiggyVue, Ziggy)
-            .use(vuetify)
             .mount(el);
     },
 
--- vite.config.js
+++ vite.config.js
@@ -19,18 +19,4 @@ export default defineConfig({
             },
         }),
     ],
-    resolve: {
-        alias: {
-        //   '@': fileURLToPath(new URL('./resources', import.meta.url)),
-        //   '@themeConfig': fileURLToPath(new URL('./themeConfig.ts', import.meta.url)),
-        //   '@core': fileURLToPath(new URL('./src/@core', import.meta.url)),
-        //   '@layouts': fileURLToPath(new URL('./src/@layouts', import.meta.url)),
-        //   '@images': fileURLToPath(new URL('./src/assets/images/', import.meta.url)),
-        //   '@styles': fileURLToPath(new URL('./src/styles/', import.meta.url)),
-        //   '@configured-variables': fileURLToPath(new URL('./src/styles/variables/_template.scss', import.meta.url)),
-        //   '@axios': fileURLToPath(new URL('./src/plugins/axios', import.meta.url)),
-        //   '@validators': fileURLToPath(new URL('./resources/js/VuexyStuff/validators', import.meta.url)),
-        //   'apexcharts': fileURLToPath(new URL('node_modules/apexcharts-clevision', import.meta.url)),
-        },
-    },
 });
