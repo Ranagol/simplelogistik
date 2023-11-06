3a69ff8bedef8e3785ad7bb5f1a3898d58e7820b
Ranagol <andorhorvat@gmail.com>
2023-11-06 21:56:53 +0100

    Small corrections.

--- resources/js/Components/InputLabel.vue
+++ resources/js/Components/InputLabel.vue
@@ -5,7 +5,7 @@ defineProps<{
 </script>
 
 <template>
-    <label class="block font-medium text-sm text-gray-700">
+    <label class="block font-medium text-sm text-color">
         <span v-if="value">{{ value }}</span>
         <span v-else><slot /></span>
     </label>
