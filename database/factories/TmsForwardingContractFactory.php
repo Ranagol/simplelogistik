c81a749386f96ad1bdcbddb0259f49b6e2aa156f
Andor Horvat <andorhorvat@gmail.com>
2023-11-15 17:05:04 +0100

    Small corrections.

---
+++ database/factories/TmsForwardingContractFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TmsForwardingContract;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tms_forwardingcontract>
+ */
+class TmsForwardingContractFactory extends Factory
+{
+    protected $model = TmsForwardingContract::class;
+    
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'internal_fcid' => $this->faker->uuid(),
+            'forwarder_id' => $this->faker->numberBetween(1, config('constants.numberOfDbRecords')),
+            'customer_id' => $this->faker->numberBetween(1, config('constants.numberOfDbRecords')),
+            'vehicle_id' => $this->faker->numberBetween(1, config('constants.numberOfDbRecords')),
+            'dispatcher_id' => $this->faker->numberBetween(1, config('constants.numberOfDbRecords')),
+            'comments' => $this->faker->sentence(),
+        ];
+    }
+}
---
+++ database/factories/TmsOfferPriceFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TmsOfferPrice;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tms_offerprice>
+ */
+class TmsOfferPriceFactory extends Factory
+{
+    protected $model = TmsOfferPrice::class;
+    
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word(),
+            'description' => $this->faker->sentence(),
+            'offer_from' => $this->faker->dateTimeBetween('-1 month', '+1 month'),
+            'offer_to' => $this->faker->dateTimeBetween('+1 month', '+2 months'),
+            'forwarder_id' => $this->faker->numberBetween(1, config('constants.numberOfDbRecords')),
+            'cargo_order_id' => $this->faker->numberBetween(1, config('constants.numberOfDbRecords')),
+            'offered_price' => $this->faker->randomFloat(2, 0, 10000),
+            'offered_price_net' => $this->faker->randomFloat(2, 0, 10000),
+        ];
+    }
+}
