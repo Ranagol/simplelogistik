c326c76ee5064a477f0e405f94e849029c79da21
Andor Horvat <andorhorvat@gmail.com>
2023-11-20 14:10:41 +0100

    Add expanding collapsing sub menus in nav.

---
+++ resources/js/Pages/Customers/CreateEditCustomer.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>CreateEditCustomer</h1>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    components: {
+    },
+    props: {
+    },
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+    },
+    methods: {
+    },
+
+});
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/Shared/LeftNavbar.vue
+++ resources/js/Shared/LeftNavbar.vue
@@ -4,7 +4,7 @@
             <!-- :class... Here we set the background, text, look of the selected nav item
             If we are one the Home page, the Home nav item will be selected, and so on.
             -->
-            <li
+            <!-- <li
                 v-for="menuItem in menuItems"
                 :key="menuItem.name"
                 class="nav_item"
@@ -13,8 +13,82 @@
                 <NavLink
                     :href="menuItem.url"
                 >{{ menuItem.name }}</NavLink>
-            </li>
+            </li> -->
         </ul>
+
+        
+
+        <!-- DYNAMIC WITH LOOPING -->
+        <el-menu
+            @open="handleOpen"
+            @close="handleClose"
+        >
+
+            <!-- HOME -->
+            <el-menu-item index="Home">
+                <NavLink
+                    href="/"
+                >Home</NavLink>
+            </el-menu-item>
+
+            <!-- LOGIN -->
+            <el-menu-item index="Login">
+                <NavLink
+                    href="/login"
+                >Login</NavLink>
+            </el-menu-item>
+
+            <!-- REGISTER -->
+            <el-menu-item index="Register">
+                <NavLink
+                    href="/register"
+                >Register</NavLink>
+            </el-menu-item>
+
+            <!-- PALMYRA -->
+            <el-menu-item index="Palmyra">
+                <NavLink
+                    href="/palmyra"
+                >Palmyra</NavLink>
+            </el-menu-item>
+            
+            <!-- CUSTOMERS -->
+            <el-sub-menu
+                index="customers"
+            >
+                <template #title>
+                    <span>Customers</span>
+                </template>
+
+                <!-- CUSTOMERS INDEX -->
+                <el-menu-item 
+                    index="customers.index"
+                >
+                    <NavLink
+                        href="/customers"
+                    >Customers</NavLink>
+                </el-menu-item>
+            </el-sub-menu>
+
+            <!-- ADDRESSES -->
+            <el-sub-menu
+                index="addresses"
+            >
+                <template #title>
+                    <span>Addresses</span>
+                </template>
+
+                <!-- ADDRESSES INDEX -->
+                <el-menu-item 
+                    index="addresses.index"
+                >
+                    <NavLink
+                        href="/addresses"
+                    >Addresses</NavLink>
+                </el-menu-item>
+            </el-sub-menu>
+
+        </el-menu>
     </Card>
 </template>
 
@@ -29,19 +103,20 @@ export default defineComponent({
     },
     data() {
         return {
-            menuItems: [
-                { name: 'Home', path: 'Home', url: '/' },
-                { name: 'Login', path: 'Login', url: '/login' },
-                { name: 'Register', path: 'Register', url: '/register' },
-                { name: 'Users', path: 'Users/Index', url: '/users' },
-                // { name: , path: 'First', url: '/first' },
-                { name: 'Pamyra', path: 'Pamyra', url: '/pamyra' },
-                { name: 'Customers', path: 'Customers/Index', url: '/customers' },
-                { name: 'Cargo orders', path: 'CargoOrders/Index', url: '/cargo-orders' },
-                { name: 'Addresses', path: 'Addresses/Index', url: '/addresses' },
-            ] as { name: string, url: string, path: string }[]
+            
+            
         }
     },
+    methods: {
+        handleOpen(key: string, keyPath: string) {
+            console.log('handleOpen');
+            console.log(key, keyPath);
+        },
+        handleClose(key: string, keyPath: string) {
+            console.log('handleClose');
+            console.log(key, keyPath);
+        }
+    }
 });
 </script>
 
