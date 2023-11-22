35c519cb241a85fc4430a221681339a995b9b76f
Andor Horvat <andorhorvat@gmail.com>
2023-11-22 05:43:50 +0100

    Customer controller refactored.

--- app/Http/Controllers/TmsCustomerController.php
+++ app/Http/Controllers/TmsCustomerController.php
@@ -6,7 +6,8 @@ use Inertia\Inertia;
 use Inertia\Response;
 use App\Models\TmsCustomer;
 use Illuminate\Http\Request;
-
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class TmsCustomerController extends Controller
 {
@@ -25,44 +26,12 @@ class TmsCustomerController extends Controller
         $page = $request->page;
         $newItemsPerPage = (int)$request->newItemsPerPage;
         
-        $customers = TmsCustomer::query()
-
-            // If there is a search term defined...
-            ->when($searchTerm, function($query, $searchTerm) {
-                $query->where('name', 'like', '%' . $searchTerm . '%');
-            })
-            
-            /**
-             * SORTING
-             * When there is $sortColumn and $sortOrder defined
-             */
-            ->when($sortColumn, function($query, $sortColumn) use ($sortOrder) {
-                $query->orderBy($sortColumn, $sortOrder);
-            }, function ($query) {
-
-                //... but if sort is not specified, please return sort by id and ascending.
-                return $query->orderBy('id', 'desc');
-            })
-
-            
-            /**
-             * PAGINATION
-             * If it is not otherwise specified, paginate by 10 items per page.
-             */
-            ->paginate($newItemsPerPage ? $newItemsPerPage : 10)
-
-            /**
-             * Include the query string too into pagination data links for page 1,2,3,4... 
-             * And the url will now include this too: http://127.0.0.1:8000/users?search=a&page=2 
-             */
-            ->withQueryString();
+        $customers = $this->getCustomers($searchTerm, $sortColumn, $sortOrder, $newItemsPerPage);
 
         $t = 8;
         return Inertia::render('Customers/Index', [
             'dataFromCustomerController' => $customers,
-            //text search filter
             'searchTermProp' => $searchTerm,
-            //sort stuff for el-table 
             'sortColumnProp' => $sortColumn,
             'sortOrderProp' => $sortOrder,
         ]);
@@ -95,7 +64,7 @@ class TmsCustomerController extends Controller
         return Inertia::location(route('customers.index'));
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, $id): SymfonyResponse
     {
         $request->validate([
             'company_name' => 'required|string|min:2|max:100',
@@ -122,34 +91,59 @@ class TmsCustomerController extends Controller
      * @param [type] $id
      * @return void
      */
-    public function destroy(Request $request, $id)
-    {
-        dd($request->all());
-        TmsCustomer::destroy($id);
-        // return redirect()->back();//nah
-        // return to_route('customers.index');//nah
-        // return redirect()->route('customers.index')->with('message', 'Customer deleted successfully');
-        // $this->index();
-        // return redirect()->route('customers.index');
-        return Inertia::location(route('customers.index'));//How to return customers with pagination data and sorting data?
-        return redirect()->back()->with('error', 'There was an error deleting the customer');//Maybe this message will appear in errors?
-    }
+    // public function destroy(Request $request, $id)
+    // {
+    //     dd($request->all());
+    //     TmsCustomer::destroy($id);
+    //     return Inertia::location(route('customers.index'));//How to return customers with pagination data and sorting data?
+    // }
 
-    public function customerDelete(Request $request)
+    /**
+     * //TODO check with Christoph if this is ok. Possible problem: using POST for delete.
+     * Because I am not sending to the backend just the customer id for deleting, I am also sending
+     * the search term, sort column and sort order. This way I can return the customers with the
+     * same search term, sort column and sort order.
+     * //TODO  check with Christoph: App\Http\Controllers\TmsCustomerController::customerDelete(): Return value must be of type Inertia\Response, Illuminate\Http\Response returned
+     *
+     * @param Request $request
+     */
+    public function customerDelete(Request $request): SymfonyResponse
     {
-        // dd($request->all());
         TmsCustomer::destroy($request->id);
-        // return redirect()->back();//nah
-        // return to_route('customers.index');//nah
-        // return redirect()->route('customers.index')->with('message', 'Customer deleted successfully');
-        // $this->index();
-        // return redirect()->route('customers.index');
         $searchTerm = $request->searchTerm;
         $sortColumn = $request->sortColumn;
         $sortOrder = $request->sortOrder;
         //pagination stuff sent from front-end
         $page = $request->page;
         $newItemsPerPage = (int)$request->newItemsPerPage;
+        $customers = $this->getCustomers($searchTerm, $sortColumn, $sortOrder, $newItemsPerPage);
+        return Inertia::location(route('customers.index', [
+            'dataFromCustomerController' => $customers,
+            'searchTermProp' => $searchTerm,
+            'sortColumnProp' => $sortColumn,
+            'sortOrderProp' => $sortOrder,
+        ]));
+
+
+        //return redirect()->back()->with('error', 'There was an error deleting the customer');//TODO Maybe this message will appear in errors?
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param string|null $searchTerm
+     * @param string|null $sortColumn
+     * @param string|null $sortOrder
+     * @param integer|null $newItemsPerPage
+     * @return LengthAwarePaginator
+     */
+    private function getCustomers(
+        string $searchTerm = null, 
+        string $sortColumn = null, 
+        string $sortOrder = null, 
+        int $newItemsPerPage = null,
+    ): LengthAwarePaginator
+    {
         $customers = TmsCustomer::query()
 
             // If there is a search term defined...
@@ -181,17 +175,9 @@ class TmsCustomerController extends Controller
              * And the url will now include this too: http://127.0.0.1:8000/users?search=a&page=2 
              */
             ->withQueryString();
-        return Inertia::location(route('customers.index', [
-            'dataFromCustomerController' => $customers,
-            //text search filter
-            'searchTermProp' => $searchTerm,
-            //sort stuff for el-table 
-            'sortColumnProp' => $sortColumn,
-            'sortOrderProp' => $sortOrder,
-        ]));
 
-
-        //return redirect()->back()->with('error', 'There was an error deleting the customer');//TODO Maybe this message will appear in errors?
+            // dd($customers);
+        return $customers;
     }
 }
 
