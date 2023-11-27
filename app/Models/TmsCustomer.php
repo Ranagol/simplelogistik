8f0569ecda31b3cea15c454f8455bf38dc87ba1b
Andor Horvat <andorhorvat@gmail.com>
2023-11-27 10:12:18 +0100

    Do soft delete from customers.

--- app/Models/TmsCustomer.php
+++ app/Models/TmsCustomer.php
@@ -15,10 +15,12 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TmsCustomer extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $guarded = ['id'];
     protected $table = "tms_customers";
