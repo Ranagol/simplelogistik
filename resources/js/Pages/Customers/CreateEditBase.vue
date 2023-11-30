f230088f195037dbcfe9d749dafd190bfcfc30b7
Andor Horvat <andorhorvat@gmail.com>
2023-11-30 14:43:06 +0100

    Customer data displayed in tabs.

--- resources/js/Pages/Customers/CreateEditBase.vue
+++ resources/js/Pages/Customers/CreateEditBase.vue
@@ -1,26 +1,100 @@
 <template>
+
+    <Head :title="customer.company_name"/>
+
     <Card>
-        <el-tabs type="border-card">
-            <el-tab-pane label="General customer data">General customer data</el-tab-pane>
-            <el-tab-pane label="Addresses">Addresses</el-tab-pane>
-            <el-tab-pane label="Contacts">Contacts</el-tab-pane>
-            <el-tab-pane label="Orders">Orders</el-tab-pane>
-            <el-tab-pane label="Offers">Offers</el-tab-pane>
-            <el-tab-pane label="Individual settings">Individual settings</el-tab-pane>
+        <h1
+            class="font-semibold text-xl text-gray-800 leading-tight mb-2"
+        >Customer: {{ customer.company_name }}</h1>
+
+        <el-tabs
+            type="border-card"
+            stretch
+        >
+            <el-tab-pane
+                label="General customer data"
+            >
+                <DataTab
+                    v-model="data.customerData"
+                />
+            </el-tab-pane>
+
+            <el-tab-pane
+                label="Addresses"
+            >
+                <AddressesTab
+                    v-model="data.customerData"
+                />
+            </el-tab-pane>
+
+            <el-tab-pane
+                label="Contacts"
+            >
+                <ContactsTab
+                    v-model="data.customerData"
+                />
+            </el-tab-pane>
+
+            <el-tab-pane
+                label="Orders"
+            >
+                <OrdersTab
+                    v-model="data.customerData"
+                />
+            </el-tab-pane>
+
+            <el-tab-pane
+                label="Offers"
+            >
+                <OffersTab
+                    v-model="data.customerData"
+                />
+            </el-tab-pane>
+
+            <el-tab-pane
+                label="Individual settings"
+            >
+                <IndividualTab
+                    v-model="data.customerData"
+                />
+            </el-tab-pane>
         </el-tabs>
     </Card>
+    
 </template>
 
 <script setup>
-import { reactive, ref, onBeforeMount } from 'vue';
+import { reactive, ref, onBeforeMount, watch } from 'vue';
 import { useCustomerStore } from '@/Stores/customerStore';
+import DataTab from '@/Pages/Customers/CustomerTabs/DataTab.vue';
+import AddressesTab from '@/Pages/Customers/CustomerTabs/AddressesTab.vue';
+import ContactsTab from '@/Pages/Customers/CustomerTabs/ContactsTab.vue';
+import OrdersTab from '@/Pages/Customers/CustomerTabs/OrdersTab.vue';
+import OffersTab from '@/Pages/Customers/CustomerTabs/OffersTab.vue';
+import IndividualTab from '@/Pages/Customers/CustomerTabs/IndividualTab.vue';
+
 
 
 const props = defineProps({
+
+    /**
+     * The customer object.
+     */
     customer: {
         type: Object,
         required: true
     }
+});
+
+watch(
+    () => props.customer, 
+    (newVal) => {
+        data.customerData = newVal;
+    }
+);
+
+const data = reactive({
+    customerData: props.customer
 });
 
 
---
+++ resources/js/Pages/Customers/CustomerTabs/DataTab.vue
@@ -0,0 +1,30 @@
+<template>
+    {{ data.customer }}
+</template>
+
+<script setup>
+import { reactive, computed, watch, onMounted, ref, onUpdated, nextTick } from 'vue';
+import { router} from '@inertiajs/vue3';//for sending requests;
+
+let props = defineProps({
+    modelValue: {
+        type: Object,
+        required: true
+    }
+});
+
+let data = reactive({
+    customer: props.modelValue
+});
+
+const emit = defineEmits(['update:modelValue']);
+function handleChange(){
+    emit('update:modelValue', 'putHereTheNewCustomerObject');
+}
+
+
+
+</script>
+
+<style scoped>
+</style>
