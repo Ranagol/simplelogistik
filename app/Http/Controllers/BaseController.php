134faaa55bc88ede3dcd3206469855d903a9e860
Andor Horvat <andorhorvat@gmail.com>
2023-12-03 08:33:59 +0100

    Contructor done.

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Model;
+
+
+abstract class BaseController extends Controller
+{
+    protected Model $model;
+    protected string $modelName;
+    protected string $vueIndexPath;
+    protected string $vueCreateEditPath;
+
+    /**
+     * Returns records.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request): Response
+    {
+        $searchTerm = $request->searchTerm;
+        $sortColumn = $request->sortColumn;
+        $sortOrder = $request->sortOrder;
+        //pagination stuff sent from front-end
+        $page = $request->page;
+        $newItemsPerPage = (int)$request->newItemsPerPage;
+        
+        $records = $this->getRecords($searchTerm, $sortColumn, $sortOrder, $newItemsPerPage);
+
+        return Inertia::render(
+            $this->vueIndexPath, 
+            [
+                'dataFromController' => $records,
+                'searchTermProp' => $searchTerm,
+                'sortColumnProp' => $sortColumn,
+                'sortOrderProp' => $sortOrder,
+            ]
+        );
+    }
+
+    public function show(string $id): void
+    {
+        $record = $this->model::find($id);
+    }
+
+    public function edit(string $id): Response
+    {
+        $record = $this->model::find($id);
+        return Inertia::render(
+            $this->vueCreateEditPath, 
+            [
+                'record' => $record,
+            ]
+        );
+    }
+
+    /**
+     * Stores records. Inertia automatically sends succes or error feedback to the frontend.
+     * 
+     * A little explanation: here we only save the record into db.
+     * This simply triggers onSuccess event in FE component, which then displays the success message
+     * to the user, and then the FE component calls the $this->index() method, which returns the records.
+     * So, the user gets his feedback, and the record list is refreshed.
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function store(Request $request): void
+    {
+        $request->validate([
+            'company_name' => 'required|string|min:2|max:100',
+            'name' => 'required|string|min:2|max:200',
+            'email' => 'required|email|max:100',
+            'rating' => 'required|integer|between:1,5',
+            'tax_number' => 'required|string|min:2|max:50',
+            'internal_cid' => 'required|string|min:2|max:100',
+        ]);
+        
+        $this->model::create($request->all());
+    }
+
+    /**
+     * Updates records. Inertia automatically sends succes or error feedback to the frontend.
+     *
+     * @param Request $request
+     * @param [type] $id
+     * @return void
+     */
+    public function update(Request $request, string $id): void
+    {
+        $request->validate([
+            'company_name' => 'required|string|min:2|max:100',
+            'name' => 'required|string|min:2|max:200',
+            'email' => 'required|email|max:100',
+            'rating' => 'required|integer|between:1,5',
+            'tax_number' => 'required|string|min:2|max:50',
+            'internal_cid' => 'required|string|min:2|max:100',
+        ]);
+        
+        $this->model::find($id)->update($request->all());
+    }
+
+    /**
+     * Deletes records. This triggers the onSuccess event in FE component, which then displays
+     * the success message to the user, and then the FE component calls the $this->index() method,
+     * which returns the records. So, the user gets his feedback, and the record list is refreshed.
+     * 
+     * @param [type] $id
+     * @return void
+     */
+    public function destroy(Request $request, string $id): void
+    {
+        $this->model::destroy($id);
+    }
+
+    /**
+     * Deletes multiple records at the same time.
+     * destroy() can be used for multiple deletes too, if you send an array of ids:
+     * https://laravel.com/docs/10.x/eloquent#deleting-an-existing-model-by-its-primary-key
+     * Example:
+     * Flight::destroy(1);
+     * Flight::destroy(1, 2, 3);
+     * Flight::destroy([1, 2, 3]);
+     * 
+     * So $id here can be an integer, or an array of integers.
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function batchDelete(Request $request): void
+    {
+        $customerIds = $request->customerIds;
+        // dd($customerIds);
+        $this->model::destroy($customerIds);
+    }
+
+    /**
+     * Returns records.
+     *
+     * @param string|null $searchTerm
+     * @param string|null $sortColumn
+     * @param string|null $sortOrder
+     * @param integer|null $newItemsPerPage
+     * @return LengthAwarePaginator
+     */
+    private function getRecords(
+        string $searchTerm = null, 
+        string $sortColumn = null, 
+        string $sortOrder = null, 
+        int $newItemsPerPage = null,
+    ): LengthAwarePaginator
+    {
+        $records = $this->model::query()
+
+            // If there is a search term defined...
+            ->when($searchTerm, function($query, $searchTerm) {
+                $query->where('name', 'like', '%' . $searchTerm . '%')
+                ->orWhere('company_name', 'like', '%' . $searchTerm . '%')
+                ->orWhere('email', 'like', '%' . $searchTerm . '%');
+            })
+            
+            /**
+             * SORTING
+             * When there is $sortColumn and $sortOrder defined
+             */
+            ->when($sortColumn, function($query, $sortColumn) use ($sortOrder) {
+                $query->orderBy($sortColumn, $sortOrder);
+            }, function ($query) {
+
+                //... but if sort is not specified, please return sort by id and ascending.
+                return $query->orderBy('id', 'desc');
+            })
+            
+            /**
+             * PAGINATION
+             * If it is not otherwise specified, paginate by 10 items per page.
+             */
+            ->paginate($newItemsPerPage ? $newItemsPerPage : 10)
+
+            /**
+             * Include the query string too into pagination data links for page 1,2,3,4... 
+             * And the url will now include this too: http://127.0.0.1:8000/users?search=a&page=2 
+             */
+            ->withQueryString();
+
+            // dd($records);
+        return $records;
+    }
+}
