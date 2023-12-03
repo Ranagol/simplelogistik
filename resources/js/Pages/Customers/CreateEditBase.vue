5642687b38dd6520070b8b20871f26a98faa5c2d
Andor Horvat <andorhorvat@gmail.com>
2023-12-03 19:02:48 +0100

    General customer data displayed.

--- resources/js/Pages/Customers/CreateEditBase.vue
+++ resources/js/Pages/Customers/CreateEditBase.vue
@@ -6,9 +6,22 @@
     />
 
     <Card>
-        <!-- <h1
-            class="font-semibold text-xl text-gray-800 leading-tight mb-2"
-        >{{ customerData.company_name }}</h1> -->
+        <div class="flex flex-row mb-3">
+            <h1
+                class="font-semibold text-xl text-gray-800 leading-tight mr-6"
+            >Edit</h1>
+
+            <span
+                v-if="data.customerData.company_name"
+                class="font-semibold text-xl text-gray-800 leading-tight mr-6"
+            >{{data.customerData.company_name }}</span>
+
+            <span
+                v-if="data.customerData.internal_cid"
+                class="font-semibold text-xl text-gray-800 leading-tight mr-6"
+            >Customer number: {{data.customerData.internal_cid }}</span>
+        </div>
+        
 
         <el-tabs
             type="border-card"
@@ -138,7 +151,12 @@ const props = defineProps({
     mode: {
         type: String,
         required: true
-    }
+    },
+
+    /**
+     * The errors object that is sent from the backend, and contains the validation errors.
+     */
+    errors: Object,
 });
 
 const data = reactive({
@@ -200,6 +218,98 @@ const editCustomer = () => {
 //         }
 //     )
 // };
+
+
+/**
+ * This contains the whole el-form. Needed for the validation.
+ */
+ const ruleFormRef = ref()
+
+/**
+ * The customer object that is being sent to the backend.
+ * This line is creating a reactive object using Vue's reactive function. The reactive function is 
+ * used to create a reactive and mutable object. The object is of type RuleForm (as defined earlier), 
+ * and it's initially set with all properties as empty strings. 
+ */
+ let customer = reactive({
+    //Use this for creating a new customer
+    // company_name: '',
+    // name: '',
+    // email: '',
+    // rating: '',
+    // tax_number: '',
+    // internal_cid: '',
+
+    //Use this to stamp customers during development
+    company_name: 'Dummy from Create',
+    name: 'sdfv',
+    email: 'bla@gmail.com',
+    rating: '5',
+    tax_number: '5555',
+    internal_cid: '66666',
+})
+
+/**
+ * The validation rules for the form.
+ */
+const rules = reactive({
+    company_name: [
+        { required: true, message: 'Company name is required FE', trigger: 'blur' },
+        { min: 3, max: 100, message: 'Length should be 3 to 100', trigger: 'blur' },
+    ],
+    name: [
+            { required: true, message: 'Name is required FE', trigger: 'blur' },
+        ],
+    email: [
+        { required: true, message: 'Email is required FE', trigger: 'blur' },
+    ],
+    rating: [
+        { required: true, message: 'Rating is required FE', trigger: 'blur' },
+    ],
+    tax_number: [
+        { required: true, message: 'Tax number is required FE', trigger: 'blur' },
+    ],
+    internal_cid: [
+        { required: true, message: 'Internal CID is required FE', trigger: 'blur' },
+    ],
+})
+
+/**
+ * Does the frontend validation, and if it is OK, then calls the submit() function.
+ */
+const emit = defineEmits(['submit']);
+const submitForm = async (formEl) => {
+    if (!formEl) return;
+
+    await formEl.validate((valid, fields) => {
+        if (valid) {//if validation is OK, then submit the customer
+            
+            customerStore.selectedCustomer = customer;
+            emit('submit');
+            
+        } else {//if validation is not OK, then show the errors
+            // console.log('FE validation not OK, error submit!', fields)
+            // console.log('customer:', customer)
+            // console.log('customerStore.selectedCustomer:', customerStore.selectedCustomer)
+        }
+    })
+}
+
+/**
+ * Close the popup, and resets the customer values in this component to empty.
+ */
+let cancel = () => {
+    customerStore.elDialogVisible = false;
+    customerStore.selectedCustomer = customerStore.customerResetValues;//resetting all customer fields
+}
+
+/**
+ * Update the selectedCustomer object in store. This is needed when the mode is 'show'
+ * or 'edit'. Not needed for create mode.
+ */
+let updateSelectedCustomer = () => {
+    customerStore.selectedCustomer = customer;
+}
 
 
 
