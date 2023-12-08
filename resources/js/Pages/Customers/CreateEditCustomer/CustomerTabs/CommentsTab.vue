f8b470325bbfb71bfca4dddc503606b985135053
Andor Horvat <andorhorvat@gmail.com>
2023-12-08 11:01:44 +0100

    Form helper BE validation works with FE valid.

--- resources/js/Pages/Customers/CreateEditCustomer/CustomerTabs/CommentsTab.vue
+++ resources/js/Pages/Customers/CreateEditCustomer/CustomerTabs/CommentsTab.vue
@@ -1,38 +1,56 @@
 <template>
 
-    <div class="w-1/2">
+    <!-- :model="data" we connect the el-form with our place, where we store form data. Thanks to
+    this, we can do prop="comment" in the el-form-item. Which is necesary for FE validation. 
+    But, in this case, data is also an Inertiajs form helper. Which is necesary and important.-->
+    <!-- :rules are the validation rules -->
+    <el-form
+        ref="elFormRef"
+        :model="data"
+        :rules="data.rules"
+        status-icon
+    >
+        <!-- Validation will not work without prop="comment" -->
+        <el-form-item
+            label="Comment"
+            prop="comment"
+        >
+            <!-- INPUT FIELD -->
+            <!-- Yes, although el-form is connected to data, prop is connected to data (these are
+                needed for validation, we still must do v-model="data.comment". Which is necesary
+                for data synchronisation.) -->
+            <el-input
+                type="textarea"
+                :rows="5"
+                placeholder="Add a comment"
+                v-model="data.comment"
+                minlength="3"
+                maxlength="500"
+                show-word-limit
+            />
 
-        <!-- INPUT FIELD -->
-        <el-input
-            type="textarea"
-            :rows="5"
-            placeholder="Add a comment"
-            v-model="data.comment"
-            minlength="3"
-            maxlength="500"
-            show-word-limit
-        />
+            <!-- BACKEND VALIDATION ERROR DISPLAY, with the help of the form helper. Remember:
+            form helper = data, because const data = useForm({... ... -->
+            <div
+                v-if="data.errors.comment"
+                v-text="data.errors.comment"
+                class="text-red-500 text-xs mt-1"
+            ></div>
+        </el-form-item>
 
-        
-    </div>
+        <el-form-item>
 
-    <!-- BACKEND VALIDATION ERROR DISPLAY -->
-    <div
-        v-if="data.errors.comment"
-        v-text="data.errors.comment"
-        class="text-red-500 text-xs mt-1"
-    ></div>
+            <!-- SUBMIT BUTTON, also must be in an el-form-item, inside el-form
+            And, it must send the elFormRef as a argument. -->
+            <el-button
+                type="primary"
+                @click="submit(elFormRef)"
+                class="mt-3"
+            >Create new comment</el-button>
 
-    <!-- SUBMIT BUTTON -->
-    <el-button
-        type="primary"
-        @click="submit"
-        class="mt-3"
-    >Create new comment</el-button>
+        </el-form-item>
+    </el-form>
 
-    <!-- <div
-        v-if="data.customer.comments"
-    >{{ data.customer.comments }}</div> -->
 
     <!-- TABLE FOR DISPLAYING ALL COMMENTS BELONGING TO THE CLIENT -->
     <el-table
@@ -62,7 +80,6 @@
 
 <script setup>
 import { reactive, computed, watch, onMounted, ref, onUpdated, nextTick } from 'vue';
-import { router} from '@inertiajs/vue3';//for sending requests;
 import { useForm } from '@inertiajs/vue3';
 
 const props = defineProps({
@@ -71,9 +88,6 @@ const props = defineProps({
         required: true,
     },
 
-    errors: {
-        type: Object,
-    },
 });
 
 watch(
@@ -82,39 +96,64 @@ watch(
     { deep: true }
 );
 
-// let data = reactive({
-//     comment: '',
-//     customer: props.customer,
-// });
+//This is necesary for validation, and it must be done with ref(), must not be done with reactive()
+const elFormRef = ref(null);
 
+/**
+ * remember: here data = form helper. So we do data.patch() instead of form.patch()
+ */
 const data = useForm({
-    comment: '',
+    comment: '',//this is the actual stuff that we do BE and FE validation. One comment.
     customer: props.customer,
+
+    /**
+     * The form fields validation rules.
+     * To turn off FE validation for comment, simply comment out the rules for comment.
+     */
+    rules: {
+        comment: [
+            { required: true, message: 'Please enter a comment FE', trigger: 'blur' },
+            { min: 3, message: 'Comment must be at least 3 characters long', trigger: 'blur' },
+            { max: 500, message: 'Comment must be at most 500 characters long', trigger: 'blur' },
+        ],
+    },
 }); 
 
-const submit = () => {
-    console.log('submit comment creating started');
-
-    data.patch(
-        `/customers/${props.customer.id}/comments/create`,
-        {
-            comment: data.comment,
-        },
-        {
-            onSuccess: () => {
-                console.log('success');
-            },
-            onError: () => {
-                console.log('error');
-                ElMessage.error('Oops, something went wrong while creating the comment.')
-                ElMessage(errors);
-            },
+/**
+ * This triggers the FE validation. It is triggered when the user clicks the submit button.
+ * 
+ */
+const submit = async (elFormRef) => {
+    if(!elFormRef){
+        return
+    }
+    await elFormRef.validate((valid, fields) => {
+        // If the FE validation is ok...
+        if (valid) {
+            console.log('submit!')
+            data.patch(//this data here is actually a form helper, that was named by me as data
+                `/customers/${props.customer.id}/comments/create`,
+                {
+                    comment: data.comment,
+                },
+                {
+                    onSuccess: () => {
+                        console.log('success');
+                    },
+                    onError: () => {
+                        console.log('error');
+                        ElMessage.error('Oops, something went wrong while creating the comment.')
+                        ElMessage(errors);
+                    },
+                }
+            );
+        } else {
+            //If the FE validation is not ok...
+            console.log('error submit!', fields);
+            ElMessage.error('Oops, something went wrong while creating the comment.')
+            ElMessage(errors);
         }
-    );
-
-    console.log('submit comment creating finished');
-
+    })
 }
-
 
 </script>
