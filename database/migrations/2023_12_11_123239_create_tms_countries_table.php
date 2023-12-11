f37b56ae2c7b421cea844324f3a7337d4e9fac7f
Andor Horvat <andorhorvat@gmail.com>
2023-12-11 14:42:45 +0100

    countries table migrate seed done.

---
+++ database/migrations/2023_12_11_123239_create_tms_countries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tms_countries', function (Blueprint $table) {
+            //Data source for countries: https://www.iban.com/country-codes
+            $table->id();
+            $table->string('country_name');
+            $table->string('alpha2_code');
+            $table->string('alpha3_code');
+            $table->string('numeric_code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tms_countries');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use App\Models\Pivot;
 use App\Models\TmsAddress;
-
+use App\Models\TmsCountry;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\TmsCustomer;
 use App\Models\TmsForwarder;
@@ -70,6 +70,7 @@ class DatabaseSeeder extends Seeder
             TmsTransportLicenseSeeder::class,
             PivotTableSeeder::class,
             TmsParcelSeeder::class,
+            TmsCountrySeeder::class,
         ]);
     }
 }
