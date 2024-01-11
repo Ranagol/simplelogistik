1921242c0d254d0e2cbb5882c1968053770b266d
Patric Schönenberger <patric@schoenenberger-media.com>
2024-01-11 07:45:27 +0100

    Update login Screen Layout and disable registration on production environment

--- config/auth.php
+++ config/auth.php
@@ -112,4 +112,5 @@ return [
 
     'password_timeout' => 10800,
 
+    'enable_registration' => env('APP_ENABLE_REGISTRATION', false),
 ];
--- resources/js/Pages/.Auth-n/Login.vue
+++ resources/js/Pages/.Auth-n/Login.vue
@@ -75,5 +75,6 @@
 </script>
 
 <style scoped>
+
 </style>
 
