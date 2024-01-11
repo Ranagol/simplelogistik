24d75e9363aec0b6f4b4949b2f56ed0e966deec7
Patric Schönenberger <patric@schoenenberger-media.com>
2024-01-11 11:11:26 +0100

    create sidebar menu in authenticated layout

--- resources/js/Shared/LeftNavbar.vue
+++ resources/js/Shared/LeftNavbar.vue
@@ -1,78 +1,41 @@
 <template>
-    <el-menu style="position: sticky !important; top: 10px !important"
-    :collapse="isCollapse"
-    @open="handleOpen"
-    @close="handleClose"
-    >
-    <el-sub-menu index="1">
-        <template #title>
-        <el-icon><location /></el-icon>
-        <span>Navigator One</span>
-        </template>
-        <el-menu-item-group>
-        <template #title><span>Group One</span></template>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-1">{{ $t("menu.left.dashboard") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-2">{{ $t("menu.left.orders") }}</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="Group Two">
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-3">{{ $t("menu.left.customers") }}</el-menu-item>
-        </el-menu-item-group>
-        <el-sub-menu index="1-4">
-            <template #title><span>item four</span></template>
-            <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="1-4-1">{{ $t("menu.left.addresses") }}</el-menu-item>
-        </el-sub-menu>
-    </el-sub-menu>
-    <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="2">
-        <el-icon><icon-menu /></el-icon>
-        <template #title>Navigator Two</template>
-    </el-menu-item>
-    <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="3">
-        <el-icon><document /></el-icon>
-        <template #title>Navigator Three</template>
-    </el-menu-item>
-    <el-menu-item @click="saveCurrentLocation('dashboard')" :class="isActive('dashboard')" index="4">
-        <el-icon><setting /></el-icon>
-        <template #title>Navigator Four</template>
-    </el-menu-item>
-    </el-menu>
-    <el-button v-model="isCollapse" @click="isCollapse = !!!isCollapse">
-        <el-icon>
-            <ArrowRight v-if="isCollapse" />
-            <ArrowLeft v-else />
+  <div class="flex flex-col h-full w-full overflow-y-scroll overflow-x-hidden hide-scrollbar bg-white px-3 pt-20">
+    <div class="w-full my-3 overflow-x-hidden text-white flex">
+      <Link class="grid place-items-center grid-flow-col" :href="route('dashboard')">
+        <el-icon class="ml-1" size="30" color="black">
+          <IconMenu></IconMenu>
         </el-icon>
-    </el-button>
+        <span class="text-black flex pl-1">{{ $t("menu.dashboard") }}</span>
+      </Link>
+    </div>
+    <div class="w-full my-3 overflow-x-hidden text-white flex">
+      <Link class="grid place-items-center grid-flow-col" :href="route('addresses.index')">
+        <el-icon class="ml-1" size="30" color="black">
+          <UserFilled></UserFilled>
+        </el-icon>
+        <span class="text-black flex pl-1">{{ $t("menu.addresses") }}</span>
+      </Link>
+    </div>
+    <div class="w-full my-3 overflow-x-hidden text-white flex">
+      <Link class="grid place-items-center grid-flow-col" :href="route('orders.index')">
+        <el-icon class="ml-1" size="30" color="black">
+          <Document></Document>
+        </el-icon>
+        <span class="text-black flex pl-1">{{ $t("menu.orders") }}</span>
+      </Link>
+    </div>
+    <div class="w-full my-3 overflow-x-hidden text-white flex">
+      <Link class="grid place-items-center grid-flow-col" href="#">
+        <el-icon class="ml-1" size="30" color="black">
+          <Location></Location>
+        </el-icon>
+        <span class="text-black flex pl-1">{{ $t("menu.more") }}</span>
+      </Link>
+    </div>
+  </div>  
 </template>
   
-  <script lang="ts" setup>
+<script lang="ts" setup>
   import { ref } from 'vue'
   import {
     ArrowRight,
@@ -81,21 +44,21 @@
     Menu as IconMenu,
     Location,
     Setting,
+    User,
+    UserFilled
   } from '@element-plus/icons-vue'
-  
-  const saveCurrentLocation = (loc: string) => {
-    sessionStorage.setItem('currentLocation', loc)
+
+</script>
+
+<style scoped>
+  /* Hide scrollbar for Chrome, Safari and Opera */
+  .hide-scrollbar::-webkit-scrollbar {
+    display: none;
   }
 
-  const isActive = (loc: string) => {
-    return sessionStorage.getItem('currentLocation') === loc ? "active" : ""
+  /* Hide scrollbar for IE, Edge and Firefox */
+  .hide-scrollbar {
+    -ms-overflow-style: none;  /* IE and Edge */
+    scrollbar-width: none;  /* Firefox */
   }
-
-  const isCollapse = ref(sessionStorage.getItem('menuStateOpen') === 'true')
-  const handleOpen = (key: string, keyPath: string[]) => {
-    sessionStorage.setItem('menuStateOpen', "true")
-  }
-  const handleClose = (key: string, keyPath: string[]) => {
-    sessionStorage.setItem('menuStateOpen', "false")
-  }
-  </script>
+</style>
