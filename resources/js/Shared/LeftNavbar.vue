1f10da9714e3145be4b01fbc599449b07ad59dfe
Patric Schönenberger <patric@schoenenberger-media.com>
2024-01-12 15:36:07 +0100

    use custom components for navigation

--- resources/js/Shared/LeftNavbar.vue
+++ resources/js/Shared/LeftNavbar.vue
@@ -1,94 +1,84 @@
 <template>
-  <div class="flex flex-col h-full w-full overflow-y-scroll overflow-x-hidden hide-scrollbar bg-white px-3 pt-20">
-    <div class="w-full my-3 overflow-x-hidden text-white flex">
-      <Link class="hoverable grid place-items-center grid-flow-col" :href="route('dashboard')">
-        <el-icon class="ml-1" size="30" color="black">
-          <Histogram></Histogram>
-        </el-icon>
-        <span class="text-black flex pl-1">{{ $t("menu.dashboard") }}</span>
-      </Link>
-    </div>
-    <div class="w-full my-3 overflow-x-hidden text-white flex">
-      <Link class="hoverable grid place-items-center grid-flow-col" :href="route('addresses.index')">
-        <el-icon class="ml-1" size="30" color="black">
-          <Memo></Memo>
-        </el-icon>
-        <span class="text-black flex pl-1">{{ $t("menu.addresses") }}</span>
-      </Link>
-    </div>
-    <div class="w-full my-3 overflow-x-hidden text-white flex">
-      <Link class="hoverable grid place-items-center grid-flow-col" :href="route('orders.index')">
-        <el-icon class="ml-1" size="30" color="black">
-          <Folder></Folder>
-        </el-icon>
-        <span class="text-black flex pl-1">{{ $t("menu.orders") }}</span>
-      </Link>
-    </div>
-    <div class="w-full my-3 overflow-x-hidden text-white flex">
-      <Link class="hoverable grid place-items-center grid-flow-col" :href="route('customers.index')">
-        <el-icon class="ml-1" size="30" color="black">
-          <UserFilled></UserFilled>
-        </el-icon>
-        <span class="text-black flex pl-1">{{ $t("menu.customers") }}</span>
-      </Link>
-    </div>
-    <div class="w-full my-3 overflow-x-hidden text-white flex flex-col">
-      <div @click="toggleSubMenu" class="hoverable grid place-items-center justify-start grid-flow-col cursor-pointer">
-        <el-icon class="ml-1" size="30" color="black">
-          <IconMenu></IconMenu>
-        </el-icon>
-        <span class="text-black flex pl-1">{{ $t("menu.integrations") }}</span>
-        <el-icon class="ml-1" size="15" color="black">
-          <ArrowDown></ArrowDown>
-        </el-icon>
-      </div>
-      <div class="submenu-floating pl-8 pt-2 overflow-y-hidden" :class="{'subMenuVisible': submenuOpen, 'subMenuHidden': !submenuOpen}">
-        <div class="w-full my-3 overflow-x-hidden text-white flex">
-          <Link class="hoverable grid place-items-center grid-flow-col" :href="route('dashboard')">
-            <el-icon class="ml-1" size="30" color="black">
-              <Location></Location>
-            </el-icon>
-            <span class="text-black flex pl-1">{{ $t("menu.pamyra") }}</span>
-          </Link>
-        </div>
-      </div>
-    </div>
-  </div>  
+  <div class="h-full transition-all duration-[300ms] custom-scroll" :class="{'max-w-[70px]': !menuOpen, 'max-w-[250px]': menuOpen}">
+    <!-- Menu Item -->
+    <SidebarMenuItem iconSize="40" :menuOpen="menuOpen" toolTipText="menu.dashboard" link="/dashboard">
+      <template #icon><Histogram /></template>
+      <template #title>{{ $t('menu.dashboard') }}</template>
+    </SidebarMenuItem>
+    <!-- Menu Item End -->
+
+    <!-- Menu Item -->
+    <SidebarMenuItem iconSize="40" :menuOpen="menuOpen" toolTipText="menu.customers" link="/customers">
+      <template #icon><Memo /></template>
+      <template #title>{{ $t('menu.customers') }}</template>
+    </SidebarMenuItem>
+    <!-- Menu Item End -->
+
+    <!-- Menu Item -->
+    <SidebarMenuItem iconSize="40" :menuOpen="menuOpen" toolTipText="menu.addresses" link="/addresses">
+      <template #icon><Folder /></template>
+      <template #title>{{ $t('menu.addresses') }}</template>
+    </SidebarMenuItem>
+    <!-- Menu Item End -->
+
+    <!-- Menu Item -->
+    <SidebarMenuItem iconSize="40" :menuOpen="menuOpen" toolTipText="menu.orders" link="/orders">
+      <template #icon><UserFilled /></template>
+      <template #title>{{ $t('menu.orders') }}</template>
+    </SidebarMenuItem>
+    <!-- Menu Item End -->
+
+    <!-- Sub Menu -->
+    <SidebarSubMenu :menuOpen="menuOpen">
+      <template #header>
+        <!-- Submenu displayed "Title" -->
+        <SidebarMenuItem :menuOpen="menuOpen" iconSize="40" link="#">
+          <template #icon><IconMenu /></template>
+          <template #title>{{ $t('menu.integrations') }}</template>
+        </SidebarMenuItem>
+      </template>
+      <!-- Submenu title End -->
+      <!-- Submenu Items -->
+      <template #items>
+        <SidebarMenuItem iconSize="40" :menuOpen="menuOpen" toolTipText="menu.pamyra" link="/pamyra">
+          <template #icon><Location /></template>
+          <template #title>{{ $t('menu.pamyra') }}</template>
+        </SidebarMenuItem>
+      </template>
+      <!-- Submenu Items End -->
+    </SidebarSubMenu>
+    <!-- Sub Menu End -->
+
+    <!-- Sidebar State Toggle Button -->
+    <button class="absolute right-0 top-1/2 -translate-y-1/2 translate-x-1/2 p-2 bg-blue-400 rounded-full h-10 w-10 grid place-items-center text-white" @click="toggleSidebar">
+      <el-icon size="20">
+        <ArrowLeft v-if="menuOpen" />
+        <ArrowRight v-else />
+      </el-icon>
+    </button>
+    <!-- Sidebar State Toggle Button End -->
+  </div>
 </template>
   
 <script lang="ts" setup>
-  import { ref } from 'vue'
-  import { Memo, User as UserFilled, Histogram, Folder, Menu as IconMenu, Location, ArrowDown } from '@element-plus/icons-vue';
-</script>
+  import { ref, defineProps} from 'vue'
+  import { Memo, User as UserFilled, Histogram, Folder, Menu as IconMenu, Location, ArrowRight, ArrowLeft } from '@element-plus/icons-vue';
+  import SidebarMenuItem from '@/Components/SidebarMenuItem.vue';
+  import SidebarSubMenu from '@/Components/SidebarSubMenu.vue';
+  
+  const props = defineProps({
+    method: Function
+  });  
 
-<script lang="ts">
-  const submenuOpen = ref(false);
-  function toggleSubMenu(){
-    submenuOpen.value = !!!submenuOpen.value;
+  const menuOpen = ref(false);
+  function toggleSidebar(){
+    props.method(!!!menuOpen.value);
+    menuOpen.value = !!!menuOpen.value;
   }
 </script>
 
 <style scoped>
-  .hoverable:hover * {
-    color: rgb(59, 130, 246)
-  }
-  .subMenuVisible {
-    max-height: 1000px;
-    transition: max-height 4000ms;
-  }
-  .subMenuHidden {
-    max-height: 1px;
-    transition: max-height 800ms;
-  }
-
-  /* Hide scrollbar for Chrome, Safari and Opera */
-  .hide-scrollbar::-webkit-scrollbar {
+  .custom-scroll::-webkit-scrollbar {
     display: none;
-  }
-
-  /* Hide scrollbar for IE, Edge and Firefox */
-  .hide-scrollbar {
-    -ms-overflow-style: none;  /* IE and Edge */
-    scrollbar-width: none;  /* Firefox */
   }
 </style>
