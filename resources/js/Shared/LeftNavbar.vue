5331f788b518bb166bdccaf8b3281bc10d2f256a
Patric Schönenberger <patric@schoenenberger-media.com>
2024-01-12 08:51:21 +0100

    add submenu, change icons

--- resources/js/Shared/LeftNavbar.vue
+++ resources/js/Shared/LeftNavbar.vue
@@ -1,56 +1,86 @@
 <template>
   <div class="flex flex-col h-full w-full overflow-y-scroll overflow-x-hidden hide-scrollbar bg-white px-3 pt-20">
     <div class="w-full my-3 overflow-x-hidden text-white flex">
-      <Link class="grid place-items-center grid-flow-col" :href="route('dashboard')">
+      <Link class="hoverable grid place-items-center grid-flow-col" :href="route('dashboard')">
         <el-icon class="ml-1" size="30" color="black">
-          <IconMenu></IconMenu>
+          <Histogram></Histogram>
         </el-icon>
         <span class="text-black flex pl-1">{{ $t("menu.dashboard") }}</span>
       </Link>
     </div>
     <div class="w-full my-3 overflow-x-hidden text-white flex">
-      <Link class="grid place-items-center grid-flow-col" :href="route('addresses.index')">
+      <Link class="hoverable grid place-items-center grid-flow-col" :href="route('addresses.index')">
         <el-icon class="ml-1" size="30" color="black">
-          <UserFilled></UserFilled>
+          <Memo></Memo>
         </el-icon>
         <span class="text-black flex pl-1">{{ $t("menu.addresses") }}</span>
       </Link>
     </div>
     <div class="w-full my-3 overflow-x-hidden text-white flex">
-      <Link class="grid place-items-center grid-flow-col" :href="route('orders.index')">
+      <Link class="hoverable grid place-items-center grid-flow-col" :href="route('orders.index')">
         <el-icon class="ml-1" size="30" color="black">
-          <Document></Document>
+          <Folder></Folder>
         </el-icon>
         <span class="text-black flex pl-1">{{ $t("menu.orders") }}</span>
       </Link>
     </div>
     <div class="w-full my-3 overflow-x-hidden text-white flex">
-      <Link class="grid place-items-center grid-flow-col" href="#">
+      <Link class="hoverable grid place-items-center grid-flow-col" :href="route('customers.index')">
         <el-icon class="ml-1" size="30" color="black">
-          <Location></Location>
+          <UserFilled></UserFilled>
         </el-icon>
-        <span class="text-black flex pl-1">{{ $t("menu.more") }}</span>
+        <span class="text-black flex pl-1">{{ $t("menu.customers") }}</span>
       </Link>
+    </div>
+    <div class="w-full my-3 overflow-x-hidden text-white flex flex-col">
+      <div @click="toggleSubMenu" class="hoverable grid place-items-center justify-start grid-flow-col cursor-pointer">
+        <el-icon class="ml-1" size="30" color="black">
+          <IconMenu></IconMenu>
+        </el-icon>
+        <span class="text-black flex pl-1">{{ $t("menu.integrations") }}</span>
+        <el-icon class="ml-1" size="15" color="black">
+          <ArrowDown></ArrowDown>
+        </el-icon>
+      </div>
+      <div class="submenu-floating pl-8 pt-2 overflow-y-hidden" :class="{'subMenuVisible': submenuOpen, 'subMenuHidden': !submenuOpen}">
+        <div class="w-full my-3 overflow-x-hidden text-white flex">
+          <Link class="hoverable grid place-items-center grid-flow-col" :href="route('dashboard')">
+            <el-icon class="ml-1" size="30" color="black">
+              <Location></Location>
+            </el-icon>
+            <span class="text-black flex pl-1">{{ $t("menu.pamyra") }}</span>
+          </Link>
+        </div>
+      </div>
     </div>
   </div>  
 </template>
   
 <script lang="ts" setup>
   import { ref } from 'vue'
-  import {
-    ArrowRight,
-    ArrowLeft,
-    Document,
-    Menu as IconMenu,
-    Location,
-    Setting,
-    User,
-    UserFilled
-  } from '@element-plus/icons-vue'
+  import { Memo, User as UserFilled, Histogram, Folder, Menu as IconMenu, Location, ArrowDown } from '@element-plus/icons-vue';
+</script>
 
+<script lang="ts">
+  const submenuOpen = ref(false);
+  function toggleSubMenu(){
+    submenuOpen.value = !!!submenuOpen.value;
+  }
 </script>
 
 <style scoped>
+  .hoverable:hover * {
+    color: rgb(59, 130, 246)
+  }
+  .subMenuVisible {
+    max-height: 1000px;
+    transition: max-height 4000ms;
+  }
+  .subMenuHidden {
+    max-height: 1px;
+    transition: max-height 800ms;
+  }
+
   /* Hide scrollbar for Chrome, Safari and Opera */
   .hide-scrollbar::-webkit-scrollbar {
     display: none;
