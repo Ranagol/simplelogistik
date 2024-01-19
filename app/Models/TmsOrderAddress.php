52fdf32a30e3b8bcab38010f35de1fe86b2403b2
Andor Horvat <andorhorvat@gmail.com>
2024-01-19 14:45:01 +0100

    Remove address_type mutator from OrderAddress.

--- app/Models/TmsOrderAddress.php
+++ app/Models/TmsOrderAddress.php
@@ -126,36 +126,4 @@ class TmsOrderAddress extends Model
         3 => 'Pickup address',
         4 => 'Delivery address',
     ];
-
-    protected function addressType(): Attribute
-    {
-        return Attribute::make(
-
-            /**
-             * Accessor
-             * gets from db, transforms it. 1 will become 'Bussiness address'.
-             * directly looks up the address type in the ADDRESS_TYPES constant using the provided
-             * value. If the value is not found in the constant, it defaults to 'Missing data.'.
-             */
-            get: fn (string $value) => self::ADDRESS_TYPES[$value] ?? 'Missing data TmsOrderAddress model.',
-
-            /**
-             * Mutator
-             * gets from request, transforms it. 'Bussiness address' will become 1.
-             *
-             * To implement the setter using the ADDRESS_TYPES constant, you need to flip the array
-             * keys and values because you're mapping from the string representation back to the
-             * integer value.
-             * he set method uses array_flip to swap the keys and values of the ADDRESS_TYPES
-             * constant, then looks up the integer value corresponding to the provided address type
-             * string. If the string is not found in the flipped array, it defaults to 'Missing data.'.
-             */
-            // set: fn (string $value) => array_flip(self::ADDRESS_TYPES)[$value] ?? 'Missing data TmsOrderAddress model.',
-            set: function (string $value) {//the old way
-                // dd($value);
-                return array_flip(self::ADDRESS_TYPES)[$value] ?? 'Missing data TmsOrderAddress model.';
-            }
-
-        );
-    }
 }
