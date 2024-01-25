0c06eac6c35857886a2c99979ec8da9de0ede94e
Andor Horvat <andorhorvat@gmail.com>
2024-01-25 11:33:25 +0100

    Create and test invoice history relationships.

--- app/Models/TmsInvoice.php
+++ app/Models/TmsInvoice.php
@@ -3,7 +3,10 @@
 namespace App\Models;
 
 use App\Models\TmsOrder;
+use App\Models\TmsCustomer;
+use App\Models\TmsInvoiceHistory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -45,5 +48,10 @@ class TmsInvoice extends Model
     public function customer(): BelongsTo
     {
         return $this->belongsTo(TmsCustomer::class);
+    }
+
+    public function invoiceHistories(): HasMany
+    {
+        return $this->hasMany(TmsInvoiceHistory::class, 'invoice_id');
     }
 }
--- app/Models/TmsInvoiceHistory.php
+++ app/Models/TmsInvoiceHistory.php
@@ -2,10 +2,43 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\TmsInvoice;
+use App\Models\TmsCustomer;
+use App\Models\TmsForwarder;
+use App\Models\TmsForwardingContract;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class TmsInvoiceHistory extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+    protected $table = "tms_invoice_histories";
+
+    public function forwarder(): BelongsTo
+    {
+        return $this->belongsTo(TmsForwarder::class);
+    }
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(TmsCustomer::class);
+    }
+
+    public function invoice(): BelongsTo
+    {
+        return $this->belongsTo(TmsInvoice::class, 'invoice_id');
+    }
+
+    public function forwardingContract(): BelongsTo
+    {
+        return $this->belongsTo(TmsForwardingContract::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
