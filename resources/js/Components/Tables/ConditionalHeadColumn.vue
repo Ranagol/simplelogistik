5d69acd12e4ae75f52eee0993e3b074f25961a91
Patric Schönenberger <p.schoenenberger@spedition.de>
2024-01-31 13:04:26 +0100

    minor fixes

--- resources/js/Components/Tables/ConditionalHeadColumn.vue
+++ resources/js/Components/Tables/ConditionalHeadColumn.vue
@@ -1,5 +1,5 @@
 <template>
-    <th v-if="data.show === true"  :key="'cell-' + Math.random() + 1" scope="col" class="px-4 py-3">
+    <th :draggable="data.draggable ?? true" v-if="data.show === true"  :key="'cell-' + Math.random() + 1" scope="col" class="px-4 py-3">
         {{ $t(data.title ?? data.text) }}
     </th>
 </template>
--- resources/js/Pages/Orders/CreateEditOrder/Partials/AddressSection.vue
+++ resources/js/Pages/Orders/CreateEditOrder/Partials/AddressSection.vue
@@ -1,8 +1,19 @@
 <template>
     <h2 @click="onToggleSection(props.section.key)" class="w-full mb-4 text-lg font-bold text-gray-900 cursor-pointer dark:text-white"><span>{{ props.title }}</span> <el-icon class="transition-all duration-200" :class="{'rotate-180' : props.sectionActive}"><ArrowDown /></el-icon></h2>
     <section class="py-3 mb-4" :class="{'border-b': props.sectionActive}">
-        <div class="grid grid-cols-4 gap-4" :class="{'hidden': !sectionActive}">
-            <div class="relative p-4 pt-8 bg-gray-200 rounded" v-for="address in tabData">
+        <div class="grid grid-cols-2 gap-4 xl:grid-cols-4" :class="{'hidden': !sectionActive}">
+            <div class="relative grid col-span-4 p-4 rounded bg-gray-50">
+                {{ $t('labels.order-customer-data') }}
+                <div class="text-corporate-400">
+                    {{  tabData?.company_name }}
+                    <p v-if="tabData?.company_name"><span :class="{'font-bold': address.company_name}">{{ tabData?.company_name }}</span></p>
+                    <p><span>{{ tabData?.first_name }} {{ tabData?.last_name }}</span></p>
+                    <p><span>{{ tabData?.street }} {{ tabData?.house_number }}</span></p>
+                    <p><span>{{ tabData?.zip_code }} {{ tabData?.city }}</span></p>
+                    <p><span>{{ tabData?.state }} {{ tabData?.country?.country_name }}</span></p>
+                </div>
+            </div>
+            <div class="relative p-4 pt-8 rounded bg-gray-50" v-for="address in tabData">
                 <span v-if="address.address_type === 'labels.address-delivery'" class="absolute block px-2 text-[12px] text-white rounded-sm top-2 right-2 bg-primary-600">
                     {{ $t(address.address_type) }}
                 </span>
@@ -17,8 +28,17 @@
                     <p><span>{{ address.state }} {{ address.country.country_name }}</span></p>
                 </div>
                 <div >
-                    Aboldatum und Uhrzeit: Zwischen {{ address.pickup_date_from }} - {{ address.pickup_date_to }}
-                    Zustelldatum und Uhrzeit: Zwischen {{ address.delivery_date_from }} - {{ address.delivery_date_to }}
+                    <p>Abholung zwischen</p>
+                    <p>
+                        {{ address.pickup_date_from }} - {{ address.pickup_date_to }}
+                    </p>
+                    <p>Zustellung zwischen</p>
+                    <p>
+                        {{ address.delivery_date_from }} - {{ address.delivery_date_to }}
+                    </p>
+                    <p>Avis</p>
+                    <p>SMS {{ address?.avis?.avis_sms ?? "nein"}}</p>
+                    <p>Anruf {{ address?.avis?.avis_phone ?? "nein" }}</p>               
                 </div>
             </div>
         </div>
