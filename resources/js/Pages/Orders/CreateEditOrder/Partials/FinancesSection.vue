61455129254c595d5cd1338cb53b761639b648eb
Patric Schönenberger <p.schoenenberger@spedition.de>
2024-02-06 14:14:36 +0100

    new component, configuration adn translation. also getting rid of some icons

--- resources/js/Pages/Orders/CreateEditOrder/Partials/FinancesSection.vue
+++ resources/js/Pages/Orders/CreateEditOrder/Partials/FinancesSection.vue
@@ -2,18 +2,9 @@
     <h2 @click="onToggleSection(props.section.key)" class="w-full mb-4 text-lg font-bold text-gray-900 cursor-pointer dark:text-white"><span>{{ props.title }}</span> <el-icon class="transition-all duration-200" :class="{'rotate-180' : props.sectionActive}"><ArrowDown /></el-icon></h2>
     <section class="py-3 mb-4" :class="{'border-b': props.sectionActive}">
         <div :class="{'hidden': !sectionActive}">
-            <div class="grid justify-between grid-flow-col">
-                <div class="">Transportkosten</div>
-                <div class="">Transportkosten</div>
-            </div>
-            <div class="grid justify-between grid-flow-col">
-                <div class="">Transportkosten</div>
-                <div class="">Transportkosten</div>
-            </div>
-            <div class="grid justify-between grid-flow-col">
-                <div class="">Transportkosten</div>
-                <div class="">Transportkosten</div>
-            </div>
+            <pre>
+                {{ data }}
+            </pre>
         </div>
     </section>
 </template>
--- resources/js/Pages/Orders/CreateEditOrder/Partials/ParcelsSection.vue
+++ resources/js/Pages/Orders/CreateEditOrder/Partials/ParcelsSection.vue
@@ -1,26 +1,26 @@
 <template>
     <h2 @click="onToggleSection(props.section.key)" class="w-full mb-4 text-lg font-bold text-gray-900 cursor-pointer dark:text-white"><span>{{ props.title }}</span> <el-icon class="transition-all duration-200" :class="{'rotate-180' : props.sectionActive}"><ArrowDown /></el-icon></h2>
     <section class="py-3 mb-4" :class="{'border-b': props.sectionActive}">
-        <div :class="{'hidden': !sectionActive}">
-            <div v-for="(parcel, index) in props.data" :key="index">
-                <div class="grid grid-cols-2 gap-4">
-                    <div class="relative grid col-span-2 p-4 mb-2 rounded bg-gray-50">
-                        <span class="font-bold text-corporate-700">{{ $t('labels.parcel') }} {{ index + 1 }}</span>
-                        <div class="mt-3">
-                            <p><span>{{ $t('labels.length') }}: {{ parcel.length }} cm</span></p>
-                            <p><span>{{ $t('labels.width') }}: {{ parcel.width }} cm</span></p>
-                            <p><span>{{ $t('labels.height') }}: {{ parcel.height }} cm</span></p>
-                            <p><span>{{ $t('labels.weight') }}: {{ parcel.weight }} kg</span></p>
-                        </div>
-                    </div>
+        <div class="grid" :class="{'hidden': !sectionActive}">
+            <button @click.prevent="addPackage" class="relative grid grid-flow-col p-3 py-2 mb-6 text-white rounded-md place-self-end bg-primary-700 place-items-center"><el-icon><Plus /></el-icon><span class="ps-2">{{ $t('labels.add-package') }}</span></button>
+            <div class="grid grid-flow-row gap-4 mb-4" v-for="(parcel, index) in parcels" :key="index">
+                <div class="grid grid-flow-col gap-4">
+                    <div class="grid place-items-center"><span>{{ index + 1 }}</span></div>
+                    <div class=""><IconTooltipInput :value="parcel.weight" placeholder="labels.weight"/></div>
+                    <div class=""><IconTooltipInput :value="parcel.height" placeholder="labels.height"/></div>
+                    <div class=""><IconTooltipInput :value="parcel.length" placeholder="labels.length"/></div>
+                    <div class=""><IconTooltipInput :value="parcel.width" placeholder="labels.width"/></div>
+                    <div class="grid place-items-center"><button @click.prevent="removePackage(index)" class="text-red-500"><el-icon size="18"><Minus /></el-icon></button></div>
                 </div>
             </div>
         </div>
     </section>
 </template>
 <script setup>
-import { ArrowDown } from '@element-plus/icons-vue';
+import IconTooltipInput from '@/Components/Inputs/IconTooltipInput.vue';
+import { ArrowDown, Minus, Plus, Remove } from '@element-plus/icons-vue';
 import { initFlowbite } from 'flowbite';
+import { reactive } from 'vue';
 import { onMounted } from 'vue';
 
 const props = defineProps({
@@ -46,6 +46,25 @@ const props = defineProps({
     }
 })
 
+var packageTemplate = {
+    weight: '',
+    height: '',
+    length: '',
+    width: ''
+}
+
+var parcels = reactive(props.data);
+
+const addPackage = () => {
+    parcels.push({...packageTemplate});
+}
+
+const removePackage = (index) => {
+    parcels.splice(index, 1);
+    if(parcels.length === 0) {
+        addPackage();
+    }
+}
 
 const onToggleSection = (section) => {
     props.onToggleSection(section);
