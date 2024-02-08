374edc09dacc6a58f4ea00f358ed0932c225717d
Andor Horvat <andorhorvat@gmail.com>
2024-02-08 16:53:51 +0100

    Writing into db works.

--- Modules/EmonsInvoice/app/Services/EmonsInvoices/EmonsInvoiceService.php
+++ Modules/EmonsInvoice/app/Services/EmonsInvoices/EmonsInvoiceService.php
@@ -55,7 +55,7 @@ class EmonsInvoiceService
         $invoices = $this->transformCsvToArray();
         // $this->checkForDuplicates($invoices);
         // $this->validateEmonsInvoices($invoices);
-        // DB::table('tms_emons_invoices')->insert($invoices);
+        DB::table('tms_emons_invoices')->insert($invoices);
         // $this->archiveFile();
     }
     
@@ -81,46 +81,39 @@ class EmonsInvoiceService
          * Every row/line is one invoice. Insid the row/line, the columns are separated by a pipe '|'.
          * So we must use the pipe as the delimiter argument for the fgetcsv function.
          */
-        while (($rowInCsvFile = $this->fgetcsvUTF8($file, null, '|')) !== false) {
+        while (($rowInCsvFile = fgetcsv($file, null, '|')) !== false) {
+
+            // $rowInCsvFile = array_map('utf8_encode', $rowInCsvFile);//TODO ANDOR ENCODING 1 (not working)this encoding is not working. We have to find a solution for this.
 
             //Every row/line in the csv file is an invoice. We will store here the invoice data.
             $invoice = [];
-
-            // dd($rowInCsvFile);
+            
 
             /**
-             * $rowInCsvFile 
-             * $rowInCsvFile[$key]
+             * $rowInCsvFile looks like this:
+             * *
+             * array:12 [
+             *     0 => "0201002145"
+             *     1 => "2023-01-16"
+             *     2 => "437560"
+             *     3 => "AUTO-PALAK - Pawel Palak"
+             *     4 => "DE"
+             *     5 => "97950"
+             *     6 => "Gro�rinderfeld [Gerchsheim]"
+             *     7 => "J�rg Schneider"
+             *     8 => "DE"
+             *     9 => "02733"
+             *     10 => "Cunewalde"
+             *     11 => "42.00"
+             *   ]
              */
-            // array:12 [
-            //     0 => "0201002145"
-            //     1 => "2023-01-16"
-            //     2 => "437560"
-            //     3 => "AUTO-PALAK - Pawel Palak"
-            //     4 => "DE"
-            //     5 => "97950"
-            //     6 => "Gro�rinderfeld [Gerchsheim]"
-            //     7 => "J�rg Schneider"
-            //     8 => "DE"
-            //     9 => "02733"
-            //     10 => "Cunewalde"
-            //     11 => "42.00"
-            //   ]
             foreach ($this->keys as $key => $value) {
-                $invoice[$value] = $rowInCsvFile[$key];
+                // $invoice[$value] = $rowInCsvFile[$key];//original working code without encoding
+                $invoice[$value] = mb_convert_encoding($rowInCsvFile[$key], 'ISO-8859-2', 'UTF-8');//TODO ANDOR ENCODING 2 (not working)
             }
 
-            /**
-             * //TODO ANDOR
-             * So, I stopped here. CSV reading and converting to php array works. The $invoices
-             * array is filled with the invoices. Next step is to write this array to the database.
-             * The utf-8 encoding is not done. Example, we have this issue here: 
-             * "Gro�rinderfeld [Gerchsheim]"
-             */
             $invoices[] = $invoice;
         }
-
-        dd($invoices);
 
         fclose($file);
 
@@ -146,47 +139,47 @@ class EmonsInvoiceService
 
     }
 
-    /**
-     * getting CSV array with UTF-8 encoding
-     *
-     * @param   resource    &$handle
-     * @param   integer     $length
-     * @param   string      $separator
-     *
-     * @return  array|false
-     */
-    private function fgetcsvUTF8(&$handle, $length, $separator = ';')
-    {
-        if (($buffer = fgets($handle, $length)) !== false)
-        {
-            $buffer = $this->autoUTF($buffer);
-            return str_getcsv($buffer, $separator);
-        }
-        return false;
-    }
+    // /**
+    //  * getting CSV array with UTF-8 encoding
+    //  *
+    //  * @param   resource    &$handle
+    //  * @param   integer     $length
+    //  * @param   string      $separator
+    //  *
+    //  * @return  array|false
+    //  */
+    // private function fgetcsvUTF8(&$handle, $length, $separator = ';')
+    // {
+    //     if (($buffer = fgets($handle, $length)) !== false)
+    //     {
+    //         $buffer = $this->autoUTF($buffer);
+    //         return str_getcsv($buffer, $separator);
+    //     }
+    //     return false;
+    // }
 
-    /**
-     * automatic convertion windows-1250 and iso-8859-2 info utf-8 string
-     *
-     * @param   string  $s
-     *
-     * @return  string
-     */
-    private function autoUTF($s)
-    {
-        return iconv('WINDOWS-1250', 'UTF-8', $s);
+    // /**
+    //  * automatic convertion windows-1250 and iso-8859-2 info utf-8 string
+    //  *
+    //  * @param   string  $s
+    //  *
+    //  * @return  string
+    //  */
+    // private function autoUTF($s)
+    // {
+    //     return iconv('WINDOWS-1250', 'UTF-8', $s);
 
-        // detect UTF-8
-        if (preg_match('#[\x80-\x{1FF}\x{2000}-\x{3FFF}]#u', $s))
-            return $s;
+    //     // detect UTF-8
+    //     if (preg_match('#[\x80-\x{1FF}\x{2000}-\x{3FFF}]#u', $s))
+    //         return $s;
 
-        // detect WINDOWS-1250
-        if (preg_match('#[\x7F-\x9F\xBC]#', $s))
-            return iconv('WINDOWS-1252', 'UTF-8', $s);
+    //     // detect WINDOWS-1250
+    //     if (preg_match('#[\x7F-\x9F\xBC]#', $s))
+    //         return iconv('WINDOWS-1252', 'UTF-8', $s);
 
-        // assume ISO-8859-2
-        return iconv('ISO-8859-2', 'UTF-8', $s);
-    }
+    //     // assume ISO-8859-2
+    //     return iconv('ISO-8859-2', 'UTF-8', $s);
+    // }
 
 
 }
