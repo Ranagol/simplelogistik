4d0accd7e01992215917058e4baf771ca9e51878
Andor Horvat <andorhorvat@gmail.com>
2024-02-10 17:11:25 +0100

    Refactor addressService.

--- app/Services/AddressService.php
+++ app/Services/AddressService.php
@@ -5,13 +5,13 @@ namespace App\Services;
 use App\Models\Address;
 use App\Models\TmsAddress;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Builder;
 
 class AddressService
 {
     /**
      * This is an ordinary array with numeric keys, and the values are the column names.
-     * This is used for the multiple column search.
-     * [ 'first_name', 'last_name']
+     * This is used for the multiple column search. [ 'first_name', 'last_name']
      *
      * @var array
      */
@@ -26,7 +26,8 @@ class AddressService
     private array $relationshipSearchColumns = [];
 
     /**
-     * Returns records for records list (Index.vue component)
+     * Returns records for records list (Index.vue component). This is the main function that
+     * triggers all other functions from this class.
      *
      * @param string|null $searchTerm
      * @param string|null $sortColumn
@@ -42,61 +43,19 @@ class AddressService
         array $searchColumns = []
     ): LengthAwarePaginator
     {
-        // $searchColumns = [
-        //     'first_name',
-        //     'last_name',
-        //     'customers__first_name',
-        //      'customers__last_name',
-        //     'forwarders__company_name',
-        // ];
 
         //Separate simple and relationship search columns into two arrays
         $this->handleSearchColumns($searchColumns);
         
-        //Define the static part of the query, but do not execute it yet.
-        $query = TmsAddress::query()
-        
-            /**
-             * SORTING
-             * When there is $sortColumn and $sortOrder defined
-             */
-            ->when($sortColumn, function($query, $sortColumn) use ($sortOrder) {
-                $query->orderBy($sortColumn, $sortOrder);
-            }, function ($query) {
-
-                //... but if sort is not specified, please return sort by id and ascending.
-                return $query->orderBy('id', 'desc');
-            });
-
-        //Add dynamically more search columns to the query
-        if ($searchTerm) {
-
-            /**
-             * 1 - DYNAMIC SEARCH - SIMPLE SEARCH INSIDE THE MODEL'S OWN TABLE
-             */
-            $query->where(function($query) use ($searchTerm) {//This adds a WHERE clause to the query.
-
-                foreach ($this->simpleSearchColumns as $column) {//For every column in the array...
-                    $query->orWhere($column, 'LIKE', "%$searchTerm%");//add an OR WHERE clause to the query.
-                }
-            });
-
-            /**
-             * 2 - DYNAMIC SEARCH - RELATIONSHIP SEARCH INSIDE RELATED TABLES
-             * This is a more complex search, because we have to search in related tables.
-             * We have to use orWhereHas() method to search in related tables.
-             */
-            foreach ($this->relationshipSearchColumns as $tableName => $column) {
-
-                $query->orWhereHas($tableName, function($query) use ($searchTerm, $column) {
-                    $query->where($column, 'LIKE', "%$searchTerm%");
-                });
-            }
-            //TODO ANDOR. The dynamic search works, but only if the FE provides the
-            //exact relationship name, and not table name.
-        }
+        //Make the dynamic query
+        $query = $this->makeDynamicQuery(
+            $searchTerm, 
+            $sortColumn, 
+            $sortOrder, 
+        );
             
         /**
+         * Do pagination.
          * Include the query string too into pagination data links for page 1,2,3,4... 
          * And the url will now include this too: http://127.0.0.1:8000/users?search=a&page=2 
          */
@@ -115,7 +74,7 @@ class AddressService
      * @param array $searchColumns
      * @return void
      */
-    private function handleSearchColumns(array $searchColumns): void
+    private function handleSearchColumns(array $searchColumns)
     {
         if (empty($searchColumns)) {
             return;
@@ -123,22 +82,111 @@ class AddressService
 
         /**
          * Loop through the array and separate the simple and relationship search columns. If the 
-         * column has one underscore, it is a simple search column. If it has more than one, it is a
-         * relationship search column.
+         * column has two underscores like __, it is a
+         * relationship search column. Example: customers__first_name. Else it is a simple search.
+         * Example: first_name.
          */
         foreach ($searchColumns as $column) {
-            if (substr_count($column, '_') === 1) {
-                $this->simpleSearchColumns[] = $column;
-            } else {
+            if (substr_count($column, '__') === 1) {
                 $result = explode('__', $column);
                 $column = $result[1];
-                $tableName = $result[0];
+                $relationshipName = $result[0];
+                $this->relationshipSearchColumns[$relationshipName] = $column;
+            } else {
+                $this->simpleSearchColumns[] = $column;
+            }
+        }
+    }
 
-                $this->relationshipSearchColumns[$tableName] = $column;
+    /**
+     * Creates a dynamic query based on the search term, sort column and sort order, and the
+     * search columns (this latter is data in which related tables in which columns should we also
+     * search.)
+     *
+     * @param string $searchTerm
+     * @param string|null $sortColumn
+     * @param string|null $sortOrder
+     * @return Builder
+     */
+    private function makeDynamicQuery(
+        string $searchTerm, 
+        string | null $sortColumn,
+        string | null $sortOrder,  
+    ): Builder
+    {
+        //Define the static part of the query, but do not execute it yet.
+        $query = TmsAddress::query()
+        
+            /**
+             * SORTING
+             */
+            ->when($sortColumn, function($query, $sortColumn) use ($sortOrder) {
+                $query->orderBy($sortColumn, $sortOrder);
+            }, function ($query) {
+
+                //... but if sort is not specified, please return sort by id and ascending.
+                return $query->orderBy('id', 'desc');
+            });
+
+        //Add dynamically more search columns to the query, if there is a search term
+        if ($searchTerm) {
+
+            //1 - DYNAMIC SEARCH - SIMPLE SEARCH INSIDE THE MODEL'S OWN TABLE
+            $this->searchSimple($query, $searchTerm);
+
+            //2 - DYNAMIC SEARCH - RELATIONSHIP SEARCH INSIDE RELATED TABLES
+            foreach ($this->relationshipSearchColumns as $relationshipName => $columnInRelatiodTable) {
+                $this->searchThroughRelationship($query, $searchTerm, $columnInRelatiodTable, $relationshipName);
             }
         }
 
-        // dd($this->simpleSearchColumns, $this->relationshipSearchColumns);
+        return $query;
+    }
+    
+    /**
+     * 1 - DYNAMIC SEARCH - SIMPLE SEARCH INSIDE THE MODEL'S OWN TABLE
+     *
+     * @param Builder $query
+     * @param string $searchTerm
+     * @return Builder
+     */
+    private function searchSimple(
+        Builder $query, 
+        string $searchTerm, 
+    ): Builder
+    {
+        //This adds a WHERE clause to the query.
+        return $query->where(function($query) use ($searchTerm) {
+
+            //For every column name in the array...
+            foreach ($this->simpleSearchColumns as $column) {
+
+                //add an OR WHERE clause to the query.
+                $query->orWhere($column, 'LIKE', "$searchTerm%");
+            }
+        });
+    }
+
+    /**
+     * 2 - DYNAMIC SEARCH - RELATIONSHIP SEARCH INSIDE RELATED TABLES
+    * This is a more complex search, because we have to search in related tables.
+    * We have to use orWhereHas() method to search in related tables.
+     *
+     * @param Builder $query
+     * @param string $searchTerm
+     * @param string $columnInRelatedTable
+     * @param string $relationshipName
+     * @return Builder
+     */
+    private function searchThroughRelationship(
+        Builder $query, 
+        string$searchTerm, 
+        string $columnInRelatedTable, 
+        string $relationshipName
+    ): Builder
+    {
+        return $query->orWhereHas($relationshipName, function($query) use ($searchTerm, $columnInRelatedTable) {
+            $query->where($columnInRelatedTable, 'LIKE', "$searchTerm%");
+        });
     }
 }
-
