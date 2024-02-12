3300a9f8cc058cd180b60597a02cf5f9150b427c
Andor Horvat <andorhorvat@gmail.com>
2024-02-12 15:36:57 +0100

    Jumping to another urgent task.

---
+++ database/migrations/2024_02_12_140524_change_order_histories.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tms_order_histories', function (Blueprint $table) {
+            if(Schema::hasColumn('tms_order_histories', 'dispatcher_id')) {
+                $table->dropColumn('dispatcher_id');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tms_order_histories', function (Blueprint $table) {
+            
+        });
+    }
+};
