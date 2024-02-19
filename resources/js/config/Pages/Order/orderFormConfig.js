993ddcea71b6388936674571abd1783e2a4d160d
Patric Schönenberger <p.schoenenberger@spedition.de>
2024-02-19 15:06:39 +0100

    feat: extract form and page configuration to separate config files

---
+++ resources/js/config/Pages/Order/orderFormConfig.js
@@ -0,0 +1,20 @@
+import addresses from '@Config/Forms/orderSections/addresses';
+import finances from '@Config/Forms/orderSections/finances';
+import general from '@Config/Forms/orderSections/general';
+import packages from '@Config/Forms/orderSections/packages';
+import vehicles from '@Config/Forms/orderSections/vehicles';
+
+export default [
+    addresses,
+    finances,
+    general,
+    packages,
+    vehicles,
+]
+    
+
+
+
+
+
+
