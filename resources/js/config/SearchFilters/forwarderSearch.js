1d37c0a8a4b8b13df8059590fcffc8c4079fa1a8
Patric Schönenberger <p.schoenenberger@spedition.de>
2024-02-19 12:06:12 +0100

    chore: remove duplicate code block for filter fields

--- resources/js/config/SearchFilters/forwarderSearch.js
+++ resources/js/config/SearchFilters/forwarderSearch.js
@@ -11,42 +11,5 @@ export default [
             { name: 'order_date', label: 'labels.order_date', type: 'date' },
             { name: 'delivery_date', label: 'labels.delivery_date', type: 'date' },
         ]
-    },
-    {
-        section: 'filter-labels.customer',
-        relation: 'customer',
-        fields: [
-            { name: 'internal_id', label: 'labels.internal_id', type: 'text' },
-            { name: 'email', label: 'labels.email', type: 'text' },
-            { name: 'company_name', label: 'labels.company', type: 'text' },
-            { name: 'first_name', label: 'labels.first_name', type: 'text' },
-            { name: 'last_name', label: 'labels.last_name', type: 'text' },
-            { name: 'zip_code', label: 'labels.zipcode', type: 'text' },
-            { name: 'city', label: 'labels.city', type: 'text' },
-        ]
-    }
-    ,{
-        section: 'filter-labels.pickup',
-        relation: 'pickupAddresses',
-        fields: [
-            { name: 'company_name', label: 'labels.company_name', type: 'text' },
-            { name: 'first_name', label: 'labels.first_name', type: 'text' },
-            { name: 'last_name', label: 'labels.last_name', type: 'text' },
-            { name: 'zip_code', label: 'labels.zipcode', type: 'text' },
-            { name: 'city', label: 'labels.city', type: 'text' },
-            { name: 'country', label: 'labels.country', type: 'text' },
-        ]
-    }
-    ,{
-        section: 'filter-labels.delivery',
-        relation: 'deliveryAddresses',
-        fields: [
-            { name: 'company_name', label: 'labels.company_name', type: 'text' },
-            { name: 'first_name', label: 'labels.first_name', type: 'text' },
-            { name: 'last_name', label: 'labels.last_name', type: 'text' },
-            { name: 'zip_code', label: 'labels.zipcode', type: 'text' },
-            { name: 'city', label: 'labels.city', type: 'text' },
-            { name: 'country', label: 'labels.country', type: 'text' },
-        ]
     }
 ]
