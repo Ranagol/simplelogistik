86ef3000efd016d85166857d3e7607f32f3e73f1
Patric Schönenberger <p.schoenenberger@spedition.de>
2024-02-21 13:15:18 +0100

    🔨 chore: update field configuration in SearchableInput component
    - Refactored the `defineProps` function to `const props = defineProps` for better code readability.
    - Added a variable `className` to store the value of `props.fieldConfig.class`.
    - Updated the class attribute in the `IconTooltipInput` component to use the `className` variable.
    - Updated the class attribute in the `CustomDropdown` component to use the `className` variable.
    - Updated the class attribute in the `SearchableInput` component to use the `className` variable.
    - Added a fallback case in the template to display a message when the field configuration is invalid.

--- resources/js/Components/Forms/FormField.vue
+++ resources/js/Components/Forms/FormField.vue
@@ -4,7 +4,7 @@ import IconTooltipInput from '@/Components/Inputs/IconTooltipInput.vue';
 import SearchableInput from '@/Components/Inputs/SearchableInput.vue';
 import { reactive } from 'vue';
 
-defineProps({
+const props = defineProps({
     fieldConfig: {
         type: Object,
         required: true
@@ -19,26 +19,27 @@ const updateValue = (value) => {
     data.currentValue = value;
 }
 
+var className = props.fieldConfig.class;
+
 </script>
 <template>
-    
-    <IconTooltipInput 
-        v-if="fieldConfig.type==='text'" 
-        :class="fieldConfig.class" 
-        :label="fieldConfig.label" 
-        :placeholder="fieldConfig.placeholder ?? fieldConfig.label" />
+    <div :class="className ?? 'col-span-6'">
+        <IconTooltipInput 
+            v-if="fieldConfig.type==='text'" 
+            :label="fieldConfig.label" 
+            :placeholder="fieldConfig.placeholder ?? fieldConfig.label" />
 
-    <CustomDropdown 
-        v-else-if="fieldConfig.type==='dropdown' || fieldConfig.type==='select'" 
-        :class="fieldConfig.class" 
-        :config="fieldConfig" />
-        
-    <SearchableInput 
-        v-if="fieldConfig.type==='searchable'" 
-        :onUpdate="updateValue" 
-        :onSelect="updateValue" 
-        :value="data.currentValue.name" 
-        :class="fieldConfig.class" 
-        :config="fieldConfig" />
+        <CustomDropdown 
+            v-else-if="fieldConfig.type==='dropdown' || fieldConfig.type==='select'" 
+            :config="fieldConfig" />
+            
+        <SearchableInput 
+            v-else-if="fieldConfig.type==='searchable'" 
+            :onUpdate="updateValue" 
+            :onSelect="updateValue" 
+            :value="data.currentValue.name" 
+            :config="fieldConfig" />
 
+        <div v-else class="text-red-400">Invalid Field Configuration</div>
+    </div>
 </template>
