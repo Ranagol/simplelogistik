b0604732631c28e7c466e6de1172de67f8ce9f09
Andor Horvat <andorhorvat@gmail.com>
2024-02-21 10:02:46 +0100

    Small fix.

--- app/Http/Resources/TmsOrderEditResource.php
+++ app/Http/Resources/TmsOrderEditResource.php
@@ -3,6 +3,8 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
+use App\Models\TmsNativeOrder;
+use App\Models\TmsPamyraOrder;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
@@ -62,9 +64,10 @@ class TmsOrderEditResource extends JsonResource
      * controller we call pamyraOrder() and nativeOrder() relationships. One of them will be null.
      * The other one will have data. We place the found data under the key 'details' in the
      * response.
-     *
+     * 
+     * @return TmsPamyraOrder | TmsNativeOrder
      */
-    private function setDetails()
+    private function setDetails(): TmsPamyraOrder | TmsNativeOrder
     {
 
         $pamyraOrder = $this->pamyraOrder ?? null;
