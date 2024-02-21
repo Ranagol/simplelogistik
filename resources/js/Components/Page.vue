56cb0f2b8c28871e78630d3f6bb7e959cada36bf
Patric Schönenberger <p.schoenenberger@spedition.de>
2024-02-21 10:13:16 +0100

    🔨 chore: update form UI components and structure to be built on top of a gonfiguration preset

---
+++ resources/js/Components/Page.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Card from '@/Shared/Card.vue';
+import TabContainer from '@Components/Tab/TabContainer.vue';
+defineProps({
+    config: {
+        type: Object,
+        required: true
+    }
+});
+</script>
+<template>
+    <Head
+        :title="$t(config.title)"
+    />
+    <Card>
+        <TabContainer v-if="config.type === 'tabs'" :config='config'  class="" />
+    </Card>
+</template>
