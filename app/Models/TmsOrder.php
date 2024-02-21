38847be05346d3bb203d829ee3da9859cfae7ed1
Andor Horvat <andorhorvat@gmail.com>
2024-02-21 16:10:45 +0100

    Create transport rule seeder.

--- app/Models/TmsOrder.php
+++ app/Models/TmsOrder.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use App\Models\TmsParcel;
-use App\Models\TmsAddress;
 use App\Models\TmsContact;
 use App\Models\TmsInvoice;
 use App\Models\TmsPartner;
@@ -15,12 +14,8 @@ use App\Models\TmsOrderAddress;
 use App\Models\TmsOrderHistory;
 use App\Models\TmsPaymentMethod;
 use App\Models\TmsOrderAttribute;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use App\Models\TmsForwardingContract;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -44,12 +39,13 @@ class TmsOrder extends Model
 
     public const TYPES_OF_TRANSPORT = [
         1 => 'General cargo',
-        2 => 'LTL/FTL',
-        3 => 'Direct transport/Direktfahrt',
-        4 => 'Parcell up to 31.5 kg',
+        2 => 'FTL / LTL',
+        3 => 'Direct transport',
+        4 => 'Parcel up to 31.5 kg',
         5 => 'Special order',
         6 => 'Regel tour',
-        7 => 'Stückgut'
+        7 => 'Stückgut',
+        8 => 'Direktfahrt'
     ];
 
     /**
@@ -226,6 +222,4 @@ class TmsOrder extends Model
             'order_attribute_id'
         );
     }
-
-
 }
