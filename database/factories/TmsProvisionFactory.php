b8b3d74153c5750771b9f68c8452663549825083
Andor Horvat <andorhorvat@gmail.com>
2024-02-22 14:09:19 +0100

    Small fix.

--- database/factories/TmsProvisionFactory.php
+++ database/factories/TmsProvisionFactory.php
@@ -17,10 +17,15 @@ class TmsProvisionFactory extends Factory
     public function definition(): array
     {
         return [
+            
+            /**
+             * There will be 3 provisions faked. These 3 provisions can easily belong to one partner.
+             * So, the number 1 here is realistic.
+             */
             'partner_id' => 1,
-            'value' => 6,//Pamyra does 6% provision
-            'valid_from' => '2023-01-01 00:00:00',
-            'valid_to' => '2025-12-31 23:59:59',
+            'value' => $this->faker->randomElement([6,8]),
+            'valid_from' => '2023-01-01',
+            'valid_to' => '2025-12-31', 
         ];
     }
 }
