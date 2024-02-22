35349bf952b3c29b95ddc8cdf782a7346a45940b
Patric Schönenberger <p.schoenenberger@spedition.de>
2024-02-22 08:53:20 +0100

    🔨 chore: add support for textarea and checkbox field types in form component

--- resources/js/Components/Forms/FormField.vue
+++ resources/js/Components/Forms/FormField.vue
@@ -40,6 +40,17 @@ var className = props.fieldConfig.class;
             :value="data.currentValue.name" 
             :config="fieldConfig" />
 
-        <div v-else class="text-red-400">Invalid Field Configuration</div>
+        <div v-else-if="fieldConfig.type==='textarea'">
+            <label for="textarea" class="block text-sm font-medium text-gray-700 dark:text-gray-300">{{ $t(fieldConfig.label) }}</label>
+            <textarea id="textarea" name="textarea" rows="3" class="block mt-1"></textarea>
+        </div>
+
+        <div v-else-if="fieldConfig.type==='checkbox' || fieldConfig.type==='check'">
+            <div class="flex items-center">
+                <input id="remember_me" name="remember_me" type="checkbox" class="w-4 h-4 border-gray-300 rounded text-primary-600 focus:ring-primary-500" />
+                <label for="remember_me" class="block ml-2">{{ $t(fieldConfig.label) }}</label>
+            </div>
+        </div>
+        <div v-else-if="fieldConfig.type==='separator'" class="h-6 col-span-12 py-6"></div>
     </div>
 </template>
