66092c290c489493721e56e0ac0811b202ca6963
Patric Schönenberger <p.schoenenberger@spedition.de>
2024-02-22 08:57:24 +0100

    🔧 fix: remove unnecessary class attribute in TabContainer component and add PageTitle component to display the title

--- resources/js/Components/Page.vue
+++ resources/js/Components/Page.vue
@@ -1,18 +1,21 @@
 <script setup>
 import Card from '@/Shared/Card.vue';
 import TabContainer from '@/Components/Tab/TabContainer.vue';
-defineProps({
+import PageTitle from '@/Components/PageTitle.vue';
+const props = defineProps({
     config: {
         type: Object,
         required: true
     }
 });
+
 </script>
 <template>
     <Head
         :title="$t(config.title)"
     />
+    <PageTitle :title="config.title" />
     <Card>
-        <TabContainer v-if="config.type === 'tabs'" :config='config'  class="" />
+        <TabContainer v-if="config.type === 'tabs'" :config='config'  />
     </Card>
 </template>
