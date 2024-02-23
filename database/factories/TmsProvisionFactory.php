56bc2bb987dead3be5691b7ba890773f090959ae
Andor Horvat <andorhorvat@gmail.com>
2024-02-23 09:04:42 +0100

    Provision seeding done.

--- database/factories/TmsProvisionFactory.php
+++ database/factories/TmsProvisionFactory.php
@@ -22,7 +22,9 @@ class TmsProvisionFactory extends Factory
              * This logic is correct, but not used. There is a different logic in the seeder.
              */
             'partner_id' => 1,
+            'sales_channel' => 'Marketplace',
             'value' => $this->faker->randomElement([6,8]),
+            'max_provision_limit_eur' => 80,
             'valid_from' => '2023-01-01',
             'valid_to' => '2025-12-31', 
         ];
