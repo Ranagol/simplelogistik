a99ac37666ec2368d648535ad05520099eb65c53
Patric Schönenberger <p.schoenenberger@spedition.de>
2024-02-26 17:34:30 +0100

    Add missing files

---
+++ resources/js/Components/Shared/Title.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1
+        class="font-semibold text-xl text-gray-500 leading-tight mr-6 mb-3"
+    >{{ props.title }}</h1>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    title: {
+        type: String,
+        required: true
+    }
+})
+
+</script>
