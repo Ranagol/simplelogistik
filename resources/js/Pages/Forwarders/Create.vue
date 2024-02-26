2baa1a7552219166065acd9db0b49e3d7b34da7d
Patric Schönenberger <p.schoenenberger@spedition.de>
2024-02-26 17:22:30 +0100

    ✨ feat: add sections and fields to customer information form
    This commit adds new sections and fields to the customer information form. The sections include fields such as ID, company name, internal ID, name, email, tax number, rating, forwarder type, and comments. These fields are necessary for capturing relevant information about the customer.

--- resources/js/Pages/Forwarders/Create.vue
+++ resources/js/Pages/Forwarders/Create.vue
@@ -4,11 +4,27 @@ import config from "@/config/Pages/Forwarders/_create";
 import Actions from "@/config/Actions";
 
 const actionHandle = new Actions(route().current(), route )
+
+const props = defineProps({
+    record: {
+        type: Object,
+        required: true
+    }
+})
+
+import { store as useStore } from "@/Stores/forwarderStore";
+
+let forwarder = useStore()
+forwarder.setOne(props.record.data)
+
 </script>
 <template>
-    <Page :page="config">
+    <Page :content="record" :store="forwarder" :page="config">
         <template #backlink>
             <button @click="actionHandle.index()">Back</button>
+        </template>
+        <template #actions>
+            <button @click="forwarder.store()">Update</button>
         </template>
     </Page>
 </template>
