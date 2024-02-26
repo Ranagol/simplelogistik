e57ea497e2851e03206d602f08a613de3e4ff682
Patric Schönenberger <p.schoenenberger@spedition.de>
2024-02-26 14:51:58 +0100

    🔧 fix: update Page component props in template
    - Update `:page` prop to `:content` prop in `Page` component.
    - Add `:store` prop to `Page` component.
    
    This change was made to ensure that the correct data is passed to the `Page` component and to include the `store` prop for proper functionality.

--- resources/js/Pages/Customers/Create.vue
+++ resources/js/Pages/Customers/Create.vue
@@ -4,9 +4,14 @@ import config from "@/config/Pages/Customers/_create";
 import Actions from "@/config/Actions";
 
 const actionHandle = new Actions(route().current(), route )
+import { store as customerStore } from "@/Stores/customerStore";
+
+let store = customerStore()
+
+store.setOne({})
 </script>
 <template>
-    <Page :page="config">
+    <Page :content="record" :store="store" :page="config">
         <template #backlink>
             <button @click="actionHandle.index()">Back</button>
         </template>
