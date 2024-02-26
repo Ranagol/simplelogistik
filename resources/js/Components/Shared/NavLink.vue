d7a3e5597001eaa2b7b9c6e2f2210c3b8ce3021d
Patric Schönenberger <p.schoenenberger@spedition.de>
2024-02-26 17:34:30 +0100

    Add missing files

---
+++ resources/js/Components/Shared/NavLink.vue
@@ -0,0 +1,15 @@
+<template>
+    <Link class="hover:underline">
+        <slot />
+    </Link>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { Link } from '@inertiajs/vue3';
+export default defineComponent({
+    components: { Link },
+});
+</script>
+
+
