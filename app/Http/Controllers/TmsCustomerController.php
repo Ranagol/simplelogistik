5787c9b4e9ed97846105bf859a011ab9e3d71131
Andor Horvat <andorhorvat@gmail.com>
2024-02-27 09:44:40 +0100

    Switch to an another urgent task.

--- app/Http/Controllers/TmsCustomerController.php
+++ app/Http/Controllers/TmsCustomerController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\GeneralResource;
-use Illuminate\Support\Facades\Session;
 use Inertia\Inertia;
 use Inertia\Response;
 use App\Models\TmsCustomer;
 use App\Models\TmsForwarder;
 use Illuminate\Http\Request;
-use App\Http\Requests\TmsCustomerRequest;
 use App\Traits\DataBaseFilter;
+use App\Http\Resources\GeneralResource;
+use Illuminate\Support\Facades\Session;
+use App\Http\Requests\TmsCustomerRequest;
+use App\Http\Requests\TmsCustomerRequest2;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Validator;
 
 class TmsCustomerController extends Controller
 {
@@ -90,17 +92,63 @@ class TmsCustomerController extends Controller
      * This simply triggers onSuccess event in FE component, which then displays the success message
      * to the user, and then the FE component calls the $this->index() method, which returns the records.
      * So, the user gets his feedback, and the record list is refreshed.
-     *
      */
+    // public function store(Request $request)
     public function store(TmsCustomerRequest $request)
+
     {
+        dd('store is triggered');
+        // dd($request->all());
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'company_name' => ['nullable', 'string', 'min:2', 'max:100'],
+                'first_name' => ['required', 'string', 'min:2', 'max:200'],
+                'last_name' => ['required', 'string', 'min:2', 'max:200'],
+                'email' => ['nullable', 'email', 'max:100'],
+                'phone' => ['required', 'string', 'min:2', 'max:100'],
+                'rating' => ['nullable', 'numeric', 'between:1,5'],
+                'tax_number' => ['nullable', 'string', 'min:2', 'max:50'],
+                'internal_id' => ['required', 'string', 'min:2', 'max:100'],
+                'payment_time' => ['nullable', 'numeric'],
+                'customer_type' => ['required'],
+                'forwarder_id' => ['nullable', 'numeric', 'exists:tms_forwarders,id'],
+                'auto_book_as_private' => ['nullable', 'boolean'],
+                'dangerous_goods' => ['nullable', 'boolean'],
+                'bussiness_customer' => ['nullable', 'boolean'],
+                'debt_collection' => ['nullable', 'boolean'],
+                'direct_debit' => ['nullable', 'boolean'],
+                'manual_collective_invoicing' => ['nullable', 'boolean'],
+                'private_customer' => ['nullable', 'boolean'],
+                'invoice_customer' => ['nullable', 'boolean'],
+                'poor_payment_morale' => ['nullable', 'boolean'],
+                'can_login' => ['nullable', 'boolean'],
+                'customer_type' => ['nullable', 'string', 'min:2', 'max:100'],
+                'invoice_dispatch' => ['nullable', 'string', 'min:2', 'max:100'],
+                'invoice_shipping_method' => ['nullable', 'string', 'min:2', 'max:100'],
+                'payment_method' => ['nullable', 'string', 'min:2', 'max:100'],
+                'payment_method_options_to_offer' => ['nullable'],
+                'email_for_invoice' => ['nullable', 'string', 'email', 'max:255'],
+                'email_for_label' => ['nullable', 'string', 'email', 'max:255'],
+                'email_for_pod' => ['nullable', 'string', 'email', 'max:255'],
+                'customer_reference' => ['nullable', 'string', 'max:255'],
+                'easy_bill_customer_id' => ['nullable', 'numeric', 'min:1'],
+            ]
+        );
+
+        if ($validator->fails()) {
+            dd($validator->errors());
+        } else {
+            dd('validation did not failed.');
+        }
+
+
         /**
          * The validated method is used to get the validated data from the request.
          */
-        $newRecord = $request->validated();//do validation
+        // $newRecord = $request->validated();//how to catch the validation issue here?
 
         $newlyCreatedRecord = TmsCustomer::create($newRecord);
-
 
         /**
          * Since a new address is created, we send a success message to the FE. First step of this
@@ -155,7 +203,7 @@ class TmsCustomerController extends Controller
      * @param string $id
      * @return void
      */
-    public function update(TmsCustomerRequest $request, string $id): void
+    public function update(TmsCustomerRequest2 $request, string $id): void
     {
         /**
          * The validated method is used to get the validated data from the request.
@@ -215,3 +263,5 @@ class TmsCustomerController extends Controller
         ]);
     }
 }
+
+
