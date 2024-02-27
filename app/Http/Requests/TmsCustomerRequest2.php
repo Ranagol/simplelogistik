477a3ceb7d75c8bff2808687a991c2793fde98d3
Andor Horvat <andorhorvat@gmail.com>
2024-02-27 09:44:40 +0100

    Switch to an another urgent task.

--- app/Http/Requests/TmsCustomerRequest2.php
+++ app/Http/Requests/TmsCustomerRequest2.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class TmsCustomerRequest extends FormRequest
+class TmsCustomerRequest2 extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -27,44 +27,18 @@ class TmsCustomerRequest extends FormRequest
     public function customerRules()
     {
         return [
-            // 'company_name' => 'nullable|string|min:2|max:100',
-            // 'first_name' => 'required|string|min:2|max:200',
-            // 'last_name' => 'required|string|min:2|max:200',
-            // 'email' => 'nullable|email|max:100',
-            // 'phone' => 'required|string|min:2|max:100',
-            // 'rating' => 'nullable|integer|between:1,5',
-            // 'tax_number' => 'nullable|string|min:2|max:50',
-            // 'internal_id' => 'required|string|min:2|max:100',
-            // 'payment_time' => 'nullable|integer',
-            // 'customer_type' => 'required',
-            // 'forwarder_id' => 'nullable|integer|exists:tms_forwarders,id',
-            // 'auto_book_as_private' => 'nullable|boolean',
-            // 'dangerous_goods' => 'nullable|boolean',
-            // 'bussiness_customer' => 'nullable|boolean',
-            // 'debt_collection' => 'nullable|boolean',
-            // 'direct_debit' => 'nullable|boolean',
-            // 'manual_collective_invoicing' => 'nullable|boolean',
-            // 'private_customer' => 'nullable|boolean',
-            // 'invoice_customer' => 'nullable|boolean',
-            // 'poor_payment_morale' => 'nullable|boolean',
-            // 'can_login' => 'nullable|boolean',
-            // 'customer_type' => 'nullable|string|min:2|max:100',
-            // 'invoice_dispatch' => 'nullable|string|min:2|max:100',
-            // 'invoice_shipping_method' => 'nullable|string|min:2|max:100',
-            // 'payment_method' => 'nullable|string|min:2|max:100',
-            // 'payment_method_options_to_offer' => 'array',
 
             'company_name' => ['nullable', 'string', 'min:2', 'max:100'],
             'first_name' => ['required', 'string', 'min:2', 'max:200'],
             'last_name' => ['required', 'string', 'min:2', 'max:200'],
             'email' => ['nullable', 'email', 'max:100'],
             'phone' => ['required', 'string', 'min:2', 'max:100'],
-            'rating' => ['nullable', 'numeric', 'between:1,5'],
+            'rating' => ['nullable', 'integer', 'between:1,5'],
             'tax_number' => ['nullable', 'string', 'min:2', 'max:50'],
             'internal_id' => ['required', 'string', 'min:2', 'max:100'],
-            'payment_time' => ['nullable', 'numeric'],
+            'payment_time' => ['nullable', 'integer'],
             'customer_type' => ['required'],
-            'forwarder_id' => ['nullable', 'numeric', 'exists:tms_forwarders,id'],
+            'forwarder_id' => ['nullable', 'integer', 'exists:tms_forwarders,id'],
             'auto_book_as_private' => ['nullable', 'boolean'],
             'dangerous_goods' => ['nullable', 'boolean'],
             'bussiness_customer' => ['nullable', 'boolean'],
@@ -72,7 +46,6 @@ class TmsCustomerRequest extends FormRequest
             'direct_debit' => ['nullable', 'boolean'],
             'manual_collective_invoicing' => ['nullable', 'boolean'],
             'private_customer' => ['nullable', 'boolean'],
-
             'invoice_customer' => ['nullable', 'boolean'],
             'poor_payment_morale' => ['nullable', 'boolean'],
             'can_login' => ['nullable', 'boolean'],
@@ -81,12 +54,11 @@ class TmsCustomerRequest extends FormRequest
             'invoice_shipping_method' => ['nullable', 'string', 'min:2', 'max:100'],
             'payment_method' => ['nullable', 'string', 'min:2', 'max:100'],
             'payment_method_options_to_offer' => ['array'],
-
             'email_for_invoice' => ['nullable', 'string', 'email', 'max:255'],
             'email_for_label' => ['nullable', 'string', 'email', 'max:255'],
             'email_for_pod' => ['nullable', 'string', 'email', 'max:255'],
             'customer_reference' => ['nullable', 'string', 'max:255'],
-            'easy_bill_customer_id' => ['nullable', 'numeric', 'min:1'],
+            'easy_bill_customer_id' => ['nullable', 'integer', 'min:1'],
         ];
     }
 }
