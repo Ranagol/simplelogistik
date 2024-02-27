33ac00d301c0a9f982bdc89958465e8fafcbe993
Patric Schönenberger <p.schoenenberger@spedition.de>
2024-02-27 07:45:40 +0100

    🔧 fix: remove commented out code and update customer form fields
    The commit removes commented out code related to salutation and title fields in the customer form. It also updates the fields in the customer form by removing the forwarder_id field and adding the company_name, internal_id, first_name, last_name, email, phone, tax_number, rating, comments, payment_time, auto_book_as_private, dangerous_goods, and business_customer fields.

--- resources/js/config/Pages/Orders/Form/_details.js
+++ resources/js/config/Pages/Orders/Form/_details.js
@@ -4,85 +4,97 @@ export default {
             title: "pages.orders.tabs.order_details",
             sectionType: "default",
             collapsible: true,
-            fields: [
+            rows: [
                 {
-                    name: 'type_of_transport',
-                    size: 'col-span-6',
-                    type: 'text',
-                    keyName: 'id',
-                    placeholder: 'pages.addresses.form.type_of_transport',
-                    label: 'pages.addresses.form.type_of_transport',
+                    className: 'grid-flow-row gap-4',
+                    fields: [
+                        {
+                            name: 'type_of_transport',
+                            type: 'text',
+                            keyName: 'id',
+                            className: 'col-auto',
+                            placeholder: 'pages.addresses.form.type_of_transport',
+                            label: 'pages.addresses.form.type_of_transport',
+                        },
+                        {
+                            name: 'order_status_id',
+                            type: 'text',
+                            className: 'col-auto',
+                            placeholder: 'pages.orders.form.order_status',
+                            label: 'pages.orders.form.order_status',
+                        },
+                    ]
                 },
                 {
-                    name: 'order_status_id',
-                    size: 'col-span-6',
-                    type: 'text',
-                    placeholder: 'pages.orders.form.order_status',
-                    label: 'pages.orders.form.order_status',
+                    className: 'grid-flow-row gap-4',
+                    fields: [
+                        {
+                            name: 'partner',
+                            className: 'col-auto',
+                            type: 'text',
+                            placeholder: 'pages.orders.form.partner',
+                            label: 'pages.orders.form.partner',
+                        },
+                        {
+                            name: 'origin',
+                            className: 'col-auto',
+                            type: 'text',
+                            placeholder: 'pages.orders.form.origin',
+                            label: 'pages.orders.form.origin',
+                        },
+                        {
+                            name: 'type_of_transport',
+                            className: 'col-auto',
+                            type: 'text',
+                            placeholder: 'pages.orders.form.first_name',
+                            label: 'pages.orders.form.first_name',
+                        },
+                        {
+                            name: 'type_of_transport',
+                            className: 'col-auto',
+                            type: 'text',
+                            placeholder: 'pages.orders.form.first_name',
+                            label: 'pages.orders.form.first_name',
+                        },
+                    ]
                 },
                 {
-                    type: 'separator',
-                    size: 'col-span-12',
+                    className: 'grid-flow-row gap-4',
+                    fields: [
+                        {
+                            name: 'type_of_transport',
+                            className: 'col-auto',
+                            type: 'text',
+                            placeholder: 'pages.orders.form.first_name',
+                            label: 'pages.orders.form.first_name',
+                        },
+                        {
+                            name: 'type_of_transport',
+                            className: 'col-auto',
+                            type: 'text',
+                            placeholder: 'pages.orders.form.first_name',
+                            label: 'pages.orders.form.first_name',
+                        },
+                    ]
                 },
                 {
-                    name: 'partner',
-                    size: 'col-span-3 w-full',
-                    type: 'text',
-                    placeholder: 'pages.orders.form.partner',
-                    label: 'pages.orders.form.partner',
-                },
-                {
-                    name: 'origin',
-                    size: 'col-span-3 w-full',
-                    type: 'text',
-                    placeholder: 'pages.orders.form.origin',
-                    label: 'pages.orders.form.origin',
-                },
-                {
-                    name: 'type_of_transport',
-                    size: 'col-span-3 w-full',
-                    type: 'text',
-                    placeholder: 'pages.orders.form.first_name',
-                    label: 'pages.orders.form.first_name',
-                },
-                {
-                    name: 'type_of_transport',
-                    size: 'col-span-3 w-full',
-                    type: 'text',
-                    placeholder: 'pages.orders.form.first_name',
-                    label: 'pages.orders.form.first_name',
-                },
-                {
-                    type: 'separator',
-                    size: 'col-span-12',
-                },
-                {
-                    name: 'type_of_transport',
-                    size: 'col-span-6',
-                    type: 'text',
-                    placeholder: 'pages.orders.form.first_name',
-                    label: 'pages.orders.form.first_name',
-                },
-                {
-                    name: 'type_of_transport',
-                    size: 'col-span-6',
-                    type: 'text',
-                    placeholder: 'pages.orders.form.first_name',
-                    label: 'pages.orders.form.first_name',
-                },
-                {
-                    name: 'type_of_transport',
-                    size: 'col-span-6',
-                    type: 'text',
-                    placeholder: 'pages.orders.form.first_name',
-                    label: 'pages.orders.form.first_name',
-                },
-                {
-                    name: 'type_of_transport',
-                    size: 'col-span-6',
-                    type: 'text',
-                    placeholder: 'pages.orders.form.first_name',
-                    label: 'pages.orders.form.first_name',
+                    className: 'grid-flow-row gap-4',
+                    fields: [
+                        {
+                            name: 'type_of_transport',
+                            className: 'col-auto',
+                            type: 'text',
+                            placeholder: 'pages.orders.form.first_name',
+                            label: 'pages.orders.form.first_name',
+                        },
+                        {
+                            name: 'type_of_transport',
+                            className: 'col-auto',
+                            type: 'text',
+                            placeholder: 'pages.orders.form.first_name',
+                            label: 'pages.orders.form.first_name',
+                        }
+                    ]
                 },
             ]
         }
