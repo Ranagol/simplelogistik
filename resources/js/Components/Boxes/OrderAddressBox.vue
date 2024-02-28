de02e85c52f356a49b502376704b0c9109abd088
Patric Schönenberger <p.schoenenberger@spedition.de>
2024-02-28 12:28:00 +0100

    🎨 style: update styling and translations in client UI
    - Updated font color in group and single item labels
    - Updated background color in a specific section of the template
    - Updated translations for badge labels
    - Added translations for address labels and general labels
    - Updated styling for badge labels in a specific section of the template

--- resources/js/Components/Boxes/OrderAddressBox.vue
+++ resources/js/Components/Boxes/OrderAddressBox.vue
@@ -15,7 +15,7 @@ defineProps({
 <template>
     <div class="grid col-span-1 p-3 pt-10 bg-slate-50" :key="index">
         <div class="relative" v-for="(item, index) in loadFields" :key="index">
-            <span v-if="item.type == 'badge'" class="absolute p-2 py-1 text-xs text-white rounded-full -top-8 -right-1.5 bg-primary-500">{{ content[item.field] }}</span>
+            <span v-if="item.type == 'badge'" class="absolute p-2 py-1 text-xs text-white rounded-full -top-8 -right-1.5 bg-primary-500">{{ $t(content[item.field]) }}</span>
             <span v-else-if="item.type === 'group'">
                 <span v-for="field in item.field">
                     {{ content[field] }}
