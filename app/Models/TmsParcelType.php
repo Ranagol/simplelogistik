fceaa3a2c224ef1aa7362299e6b2495850f752c9
Andor Horvat <andorhorvat@gmail.com>
2024-02-29 15:13:25 +0100

    Add booleans and nested array to parcel types.

--- app/Models/TmsParcelType.php
+++ app/Models/TmsParcelType.php
@@ -13,7 +13,7 @@ class TmsParcelType extends Model
     protected $guarded = ['id'];
     protected $table = "tms_parcel_types";
 
-    public const PARCEL_TYPES = [
+    public const PARCEL_TYPES = [//OLD, to be removed
         'bulky goods', 
         'euro pallet', 
         'one-way pallet',
@@ -21,8 +21,70 @@ class TmsParcelType extends Model
         'pallet cage'
     ];
 
+    private static $allParcelTypes = [//NEW
+        'bulky goods' => [
+            'display' => false
+        ],
+        'euro pallet' => [
+            'display' => true
+        ],
+        'one-way pallet' => [
+            'display' => true
+        ],
+        'cardboard' => [
+            'display' => false
+        ],
+        'pallet cage' => [
+            'display' => false
+        ]
+    ];
+
+    //******************RELATIONSHIPS********************** */
+
+
     public function parcels(): HasMany
     {
         return $this->hasMany(TmsParcel::class, 'parcel_type_id');
     }
+
+
+    //******************GETTERS********************** */
+
+    /**
+     * Get the value of allParcelTypes. Althoug this is a nested array, we return a simple array of
+     * strings, which are the parcel types.
+     * 
+     * @return array
+     */ 
+    public static function getAllParcelTypes(): array
+    {
+        $allParcelTypes = [];
+
+        foreach (self::$allParcelTypes as $parcelType => $value) {
+            $allParcelTypes[] = $parcelType;
+        }
+
+        return $allParcelTypes;
+    }
+
+    /**
+     * Get the value of only those parcel types that have 'true' in the 'display' key. True in this 
+     * case is for, yes, display the parcel type in the front-end.
+     * Althoug this is a nested array, we return a simple array of
+     * strings, which are the parcel types.
+     * 
+     * @return array
+     */
+    public static function getFrontendParcelTypes(): array
+    {
+        $frontendParcelTypes = [];
+
+        foreach (self::$allParcelTypes as $parcelType => $value) {
+            if ($value['display'] === true) {
+                $frontendParcelTypes[] = $parcelType;
+            }
+        }
+        return $frontendParcelTypes;
+    }
+
 }
