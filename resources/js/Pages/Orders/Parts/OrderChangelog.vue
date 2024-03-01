9b9894ec7ca6c9cffbae20f8379b97a0a1fef37b
Patric Schönenberger <p.schoenenberger@spedition.de>
2024-03-01 13:38:48 +0100

    🐛 fix: update translations and fix history items rendering issue
    The translations for the "download_transport_details" key have been updated to include a new translation for the "messages.changed_state_to" key. Additionally, a fix has been made to correctly render the history items in the template.

--- resources/js/Pages/Orders/Parts/OrderChangelog.vue
+++ resources/js/Pages/Orders/Parts/OrderChangelog.vue
@@ -9,33 +9,44 @@
     });
 
     import {store as useStore} from '@/Stores/orderStore';
-    import { reactive } from 'vue';
+import { formItemValidateStates } from 'element-plus';
+    import { reactive, ref } from 'vue';
     
     let contentStore = useStore();
     let content = contentStore.getOne();
     
-    let history = reactive({
-        items: []
-    });
-
-    const getStatus = async (id) => {
-        let response = await axios.get(`/api/order-status/${id}`)
-        history.items[id].name = response.data.description    
+    const getStatus = (id, callback) => {
+        axios.get("/api/order-status/" + id).then((response) => {
+            callback(response.data);
+        });
     }
 
+    let historyItems = ref([]);
+    
+    const fetchHistoryStates = () => {
+        var hist = content.history.map((item) => {
+            getStatus(item.order_status_id, (data) => {
+                item.status = data;
+            });
+
+            return item
+        });
+
+        historyItems = ref(hist)
+    }
+
+    fetchHistoryStates();
 </script>
 <template>
-    <ol class="relative border-gray-200 border-s dark:border-gray-700">               
-        
-        <li v-for="item,index in content.history" :key="index" class="mb-10 ms-6">    
-            {{ getStatus(item.order_status_id) }}        
+    <ol class="relative border-gray-200 border-s dark:border-gray-700">
+        <li v-for="item,index in historyItems" :key="index" class="mb-10 ms-6">
             <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
                 <img class="rounded-full shadow-lg" :src="item.user?.profile_image_url ?? '/images/avatar/profile.svg'" :alt="item.user.name"/>
             </span>
             <div class="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600">
                 <div class="items-center justify-between mb-3 sm:flex">
                     <time class="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">{{ moment(item.created_at).fromNow() }}</time>
-                    <div class="text-sm font-normal text-gray-500 lex dark:text-gray-300">{{ item.user.name }}  <a href="#" class="font-semibold text-gray-900 dark:text-white hover:underline">{{ history.items[id].name }}</a></div>
+                    <div class="text-sm font-normal text-gray-500 lex dark:text-gray-300">{{ item?.user?.name }} {{ $t("labels.general.messages.changed_state_to") }}: <a href="#" class="font-semibold text-gray-900 dark:text-white hover:underline">{{  item?.status?.description  }}</a></div>
                 </div>
                 <div class="p-3 text-xs italic font-normal text-gray-500 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-600 dark:border-gray-500 dark:text-gray-300">{{ item.details}}</div>
             </div>
