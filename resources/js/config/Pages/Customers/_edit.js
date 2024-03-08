270fb612b3f0028c9a96afa4561da982df524250
Patric Schönenberger <p.schoenenberger@spedition.de>
2024-03-08 08:58:03 +0100

    🚀 feat: refactor customer edit page to use tabs layout for improved user experience

--- resources/js/config/Pages/Customers/_edit.js
+++ resources/js/config/Pages/Customers/_edit.js
@@ -1,7 +1,72 @@
-import _config from "./Form/_config";
+import { Notebook, Bell, Location, FolderOpened, User, Sort } from "@element-plus/icons-vue"
+
+import Details from "@/Pages/Customers/Parts/Details.vue"
+import Preferences from "@/Pages/Customers/Parts/Preferences.vue"
+import Comments from "@/Pages/Customers/Parts/Comments.vue"
+import Addresses from "@/Pages/Customers/Parts/Addresses.vue"
+import Contacts from "@/Pages/Customers/Parts/Contacts.vue"
+import Orders from "@/Pages/Customers/Parts/Orders.vue"
+import Offers from "@/Pages/Customers/Parts/Offers.vue"
+
 export default {
-    title: "pages.customers.edit",
-    preset: "form",
+    title: "",
+    preset: "tabs",
     mode: "edit",
-    form: _config
+    actions: [
+        {handler: (data) => {
+        }, className: "bg-primary-700 text-white hover:bg-primary-600", title: "buttons.general.save"},
+            {handler: (data) => {
+        }, className: "bg-red-700 text-white hover:bg-red-600", title: "buttons.general.void"},
+    ],
+    tabs: [
+        {
+            "title": "pages.customers.tabs.details",
+            "id": "customers_Details",
+            "icon": Notebook,
+            "component": Details,
+            "useContent": true,
+        },
+        {
+            "title": "pages.customers.tabs.preferences",
+            "id": "customers_Preferences",
+            "icon": Bell,
+            "component": Preferences,
+            "useContent": true, 
+        },
+        {
+            "title": "pages.customers.tabs.comments",
+            "id": "customers_Comments",
+            "icon": Location,
+            "component": Comments,
+            "useContent": true, 
+        },
+        {
+            "title": "pages.customers.tabs.addresses",
+            "id": "customers_Addresses",
+            "icon": FolderOpened,
+            "component": Addresses,
+            "useContent": true, 
+        },
+        {
+            "title": "pages.customers.tabs.contacts",
+            "id": "customers_Contacts",
+            "icon": User,
+            "component": Contacts,
+            "useContent": true, 
+        },
+        {
+            "title": "pages.customers.tabs.orders",
+            "id": "customers_Orders",
+            "icon": Sort,
+            "component": Orders,
+            "useContent": true, 
+        },
+        {
+            "title": "pages.customers.tabs.offers",
+            "id": "customers_Offers",
+            "icon": Sort,
+            "component": Offers,
+            "useContent": true, 
+        },
+    ]
 }
