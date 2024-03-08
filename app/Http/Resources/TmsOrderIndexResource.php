0dcb236c41e8cfe48fe47d95d19cb310e55eddd8
Andor Horvat <andorhorvat@gmail.com>
2024-03-08 10:23:22 +0100

    Address issue fixed.

--- app/Http/Resources/TmsOrderIndexResource.php
+++ app/Http/Resources/TmsOrderIndexResource.php
@@ -176,7 +176,14 @@ class TmsOrderIndexResource extends JsonResource
          */
         if($addresses->count() > 1) {
             $firstPickupAddress = $addresses->first();
-            $zipAndCity .=  ' +' . ($addresses->count() - 1);
+            $zipAndCity .=  $firstPickupAddress
+                                    ->country
+                                    ->alpha2_code
+                            . ' '
+                            . $firstPickupAddress->zip_code
+                            . ' ' 
+                            . $firstPickupAddress->city
+                            . ' +' . ($addresses->count() - 1);
         }
 
         return $zipAndCity;
